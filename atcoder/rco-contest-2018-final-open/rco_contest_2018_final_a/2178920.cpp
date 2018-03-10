66b159e22b1ff3dc3e4273df788c55c8418204b3
ecto0310 <ecto0310@gmail.com>
2018-03-10 08:25:20 +0000

    Add atcoder/rco-contest-2018-final-open/rco_contest_2018_final_a/2178920.cpp

---
+++ atcoder/rco-contest-2018-final-open/rco_contest_2018_final_a/2178920.cpp
@@ -0,0 +1,15 @@
+// URL: https://atcoder.jp/contests/rco-contest-2018-final-open/submissions/2178920
+// Date: Sat, 10 Mar 2018 08:25:20 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+  int N,K;
+  cin>> N>> K;
+  
+  for(int i=0;i<K;i++)cout<<"-1 -1"<< endl;
+  return 0;
+}
