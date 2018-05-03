60f28bbd496e71c83c31b3f3fb05b319af31d14f
ecto0310 <ecto0310@gmail.com>
2018-05-03 04:35:27 +0000

    Add atcoder/abc077/abc077_b/2448996.cpp

---
+++ atcoder/abc077/abc077_b/2448996.cpp
@@ -0,0 +1,18 @@
+// URL: https://atcoder.jp/contests/abc077/submissions/2448996
+// Date: Thu, 03 May 2018 04:35:27 +0000
+// Language: C++14 (GCC 5.4.1)
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int n;
+	cin >> n;
+	cout << (int)pow((int)sqrt((double)n),2) << endl;
+
+	return 0;
+}
