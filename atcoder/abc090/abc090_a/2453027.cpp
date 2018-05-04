58ba6d2e72fa6a96d03f5fa2573a88dfbb9af1fd
ecto0310 <ecto0310@gmail.com>
2018-05-04 04:49:39 +0000

    Add atcoder/abc090/abc090_a/2453027.cpp

---
+++ atcoder/abc090/abc090_a/2453027.cpp
@@ -0,0 +1,20 @@
+// URL: https://atcoder.jp/contests/abc090/submissions/2453027
+// Date: Fri, 04 May 2018 04:49:39 +0000
+// Language: C++14 (GCC 5.4.1)
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int a[200000], b[200000];
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	string c[3];
+	cin >> c[0] >> c[1] >> c[2];
+	cout << c[0][0] << c[1][1] << c[2][2] << endl;
+
+	return 0;
+}
