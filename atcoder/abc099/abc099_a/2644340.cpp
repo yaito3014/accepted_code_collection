c3daac8b65dd6fe9ebdb5ba1d995093f10b0600c
ecto0310 <ecto0310@gmail.com>
2018-06-10 12:02:41 +0000

    Add atcoder/abc099/abc099_a/2644340.cpp

---
+++ atcoder/abc099/abc099_a/2644340.cpp
@@ -0,0 +1,17 @@
+// URL: https://atcoder.jp/contests/abc099/submissions/2644340
+// Date: Sun, 10 Jun 2018 12:02:41 +0000
+// Language: C++14 (GCC 5.4.1)
+#include<bits/stdc++.h>
+#include<assert.h>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	if(n < 1000)cout << "ABC" << endl;
+	else cout << "ABD"<< endl;
+
+	return 0;
+}
