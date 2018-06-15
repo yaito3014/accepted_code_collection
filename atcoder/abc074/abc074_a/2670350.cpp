fe95d87a8ed570b67516c3b5beb670c54420c183
ecto0310 <ecto0310@gmail.com>
2018-06-15 11:18:09 +0000

    Add atcoder/abc074/abc074_a/2670350.cpp

---
+++ atcoder/abc074/abc074_a/2670350.cpp
@@ -0,0 +1,16 @@
+// URL: https://atcoder.jp/contests/abc074/submissions/2670350
+// Date: Fri, 15 Jun 2018 11:18:09 +0000
+// Language: C++14 (GCC 5.4.1)
+#include<bits/stdc++.h>
+#include<assert.h>
+
+using namespace std;
+
+int main()
+{
+	int n, a;
+	cin >> n >> a;
+	cout << n * n - a << endl;
+
+	return 0;
+}
