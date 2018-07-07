c4cbd7b28ee50fe42b3bf86855390cf551615e9a
ecto0310 <ecto0310@gmail.com>
2018-07-07 06:01:56 +0000

    Add atcoder/abc068/abc068_a/2799600.cpp

---
+++ atcoder/abc068/abc068_a/2799600.cpp
@@ -0,0 +1,16 @@
+// URL: https://atcoder.jp/contests/abc068/submissions/2799600
+// Date: Sat, 07 Jul 2018 06:01:56 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+#include <assert.h>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cin>>n;
+	cout<< "ABC"<<n<< endl;
+
+	return 0;
+}
