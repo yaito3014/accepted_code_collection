b74ddb0914931a4e6ecadbc5af1baf719e5acaf1
ecto0310 <ecto0310@gmail.com>
2018-07-09 13:49:55 +0000

    Add atcoder/abc043/abc043_a/2818951.cpp

---
+++ atcoder/abc043/abc043_a/2818951.cpp
@@ -0,0 +1,16 @@
+// URL: https://atcoder.jp/contests/abc043/submissions/2818951
+// Date: Mon, 09 Jul 2018 13:49:55 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+#include <assert.h>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	cout << n * (n + 1) / 2 << endl;
+
+	return 0;
+}
