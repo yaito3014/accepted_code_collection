51aad07153a7f382f80ee020ee79bc2f93454090
ecto0310 <ecto0310@gmail.com>
2018-07-09 13:44:26 +0000

    Add atcoder/abc045/abc045_a/2818938.cpp

---
+++ atcoder/abc045/abc045_a/2818938.cpp
@@ -0,0 +1,16 @@
+// URL: https://atcoder.jp/contests/abc045/submissions/2818938
+// Date: Mon, 09 Jul 2018 13:44:26 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+#include <assert.h>
+
+using namespace std;
+
+int main()
+{
+	int a, b, h;
+	cin >> a >> b >> h;
+	cout << (a + b) * h / 2 << endl;
+
+	return 0;
+}
