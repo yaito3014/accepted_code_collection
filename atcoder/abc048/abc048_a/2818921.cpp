732d73f19ea7391b0f0adec02159859afc1b6b79
ecto0310 <ecto0310@gmail.com>
2018-07-09 13:39:39 +0000

    Add atcoder/abc048/abc048_a/2818921.cpp

---
+++ atcoder/abc048/abc048_a/2818921.cpp
@@ -0,0 +1,16 @@
+// URL: https://atcoder.jp/contests/abc048/submissions/2818921
+// Date: Mon, 09 Jul 2018 13:39:39 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+#include <assert.h>
+
+using namespace std;
+
+int main()
+{
+	string tmp, s;
+	cin >> tmp >> s >> tmp;
+	cout << "A" << s[0] << "C" << endl;
+
+	return 0;
+}
