9aa26b4786c81214b74a572dda6ecd62bd8b3fbb
ecto0310 <ecto0310@gmail.com>
2018-07-09 13:29:17 +0000

    Add atcoder/abc056/abc056_a/2818865.cpp

---
+++ atcoder/abc056/abc056_a/2818865.cpp
@@ -0,0 +1,19 @@
+// URL: https://atcoder.jp/contests/abc056/submissions/2818865
+// Date: Mon, 09 Jul 2018 13:29:17 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+#include <assert.h>
+
+using namespace std;
+
+int main()
+{
+	string a, b;
+	cin >> a >> b;
+	if (a == b)
+		cout << "H" << endl;
+	else
+		cout << "D" << endl;
+
+	return 0;
+}
