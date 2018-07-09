16c82753753b458050bfa087587997eb6b118470
ecto0310 <ecto0310@gmail.com>
2018-07-09 13:36:00 +0000

    Add atcoder/abc051/abc051_a/2818902.cpp

---
+++ atcoder/abc051/abc051_a/2818902.cpp
@@ -0,0 +1,18 @@
+// URL: https://atcoder.jp/contests/abc051/submissions/2818902
+// Date: Mon, 09 Jul 2018 13:36:00 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+#include <assert.h>
+
+using namespace std;
+
+int main()
+{
+	string s;
+	cin >> s;
+	s[5] = ' ';
+	s[13] = ' ';
+	cout << s << endl;
+
+	return 0;
+}
