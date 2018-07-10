bfc6fdf3f0c5cd4fa1830251f953535098341dbe
ecto0310 <ecto0310@gmail.com>
2018-07-10 07:41:02 +0000

    Add atcoder/code-festival-2017-qualb/code_festival_2017_qualb_a/2821358.cpp

---
+++ atcoder/code-festival-2017-qualb/code_festival_2017_qualb_a/2821358.cpp
@@ -0,0 +1,16 @@
+// URL: https://atcoder.jp/contests/code-festival-2017-qualb/submissions/2821358
+// Date: Tue, 10 Jul 2018 07:41:02 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+#include <assert.h>
+
+using namespace std;
+
+int main()
+{
+	string s;
+	cin >> s;
+	cout << s.substr(0, s.size() - 8)<< endl;
+
+	return 0;
+}
