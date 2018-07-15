d44471219f81ee2668dde1763974b4cb9dd89af6
ecto0310 <ecto0310@gmail.com>
2018-07-15 14:19:16 +0000

    Add atcoder/abc049/abc049_b/2853129.cpp

---
+++ atcoder/abc049/abc049_b/2853129.cpp
@@ -0,0 +1,23 @@
+// URL: https://atcoder.jp/contests/abc049/submissions/2853129
+// Date: Sun, 15 Jul 2018 14:19:16 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+#include <assert.h>
+
+using namespace std;
+
+int main()
+{
+	int h, w;
+	cin >> h >> w;
+	for (int i = 0; i < h; i++)
+	{
+		string c;
+		cin >> c;
+		cout << c << "
+"
+				 << c << endl;
+	}
+
+	return 0;
+}
