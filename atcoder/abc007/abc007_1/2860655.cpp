8f03dea56e5c6d57af0820c9872f9736a83cbde5
ecto0310 <ecto0310@gmail.com>
2018-07-17 13:16:23 +0000

    Add atcoder/abc007/abc007_1/2860655.cpp

---
+++ atcoder/abc007/abc007_1/2860655.cpp
@@ -0,0 +1,16 @@
+// URL: https://atcoder.jp/contests/abc007/submissions/2860655
+// Date: Tue, 17 Jul 2018 13:16:23 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+#include <assert.h>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	cout << n - 1 << endl;
+
+	return 0;
+}
