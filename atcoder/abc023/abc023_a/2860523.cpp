7294dc580147d6dab417f2e1efd68d04eb5a900a
ecto0310 <ecto0310@gmail.com>
2018-07-17 12:50:51 +0000

    Add atcoder/abc023/abc023_a/2860523.cpp

---
+++ atcoder/abc023/abc023_a/2860523.cpp
@@ -0,0 +1,16 @@
+// URL: https://atcoder.jp/contests/abc023/submissions/2860523
+// Date: Tue, 17 Jul 2018 12:50:51 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+#include <assert.h>
+
+using namespace std;
+
+int main()
+{
+	int x;
+	cin >> x;
+	cout << x / 10 + x % 10 << endl;
+
+	return 0;
+}
