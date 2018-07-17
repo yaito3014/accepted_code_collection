608b62a6864881d027d6aaac84969fd74b7fb8bd
ecto0310 <ecto0310@gmail.com>
2018-07-17 13:07:29 +0000

    Add atcoder/abc015/abc015_1/2860611.cpp

---
+++ atcoder/abc015/abc015_1/2860611.cpp
@@ -0,0 +1,19 @@
+// URL: https://atcoder.jp/contests/abc015/submissions/2860611
+// Date: Tue, 17 Jul 2018 13:07:29 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+#include <assert.h>
+
+using namespace std;
+
+int main()
+{
+	string a, b;
+	cin >> a >> b;
+	if (a.length() > b.length())
+		cout << a << endl;
+	else
+		cout << b << endl;
+
+	return 0;
+}
