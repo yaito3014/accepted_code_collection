0b776596728e0c17e5f3c363001eca7446210230
ecto0310 <ecto0310@gmail.com>
2018-07-17 14:10:37 +0000

    Add atcoder/abc041/abc041_b/2860897.cpp

---
+++ atcoder/abc041/abc041_b/2860897.cpp
@@ -0,0 +1,16 @@
+// URL: https://atcoder.jp/contests/abc041/submissions/2860897
+// Date: Tue, 17 Jul 2018 14:10:37 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+#include <assert.h>
+
+using namespace std;
+
+int main()
+{
+	long long a, b, c;
+	cin >> a >> b >> c;
+	cout << a * b % 1000000007 * c % 1000000007 << endl;
+
+	return 0;
+}
