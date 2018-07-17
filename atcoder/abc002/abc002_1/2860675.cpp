5afe2ef5ff86591c6175f99e1304d331eb6e90db
ecto0310 <ecto0310@gmail.com>
2018-07-17 13:21:58 +0000

    Add atcoder/abc002/abc002_1/2860675.cpp

---
+++ atcoder/abc002/abc002_1/2860675.cpp
@@ -0,0 +1,16 @@
+// URL: https://atcoder.jp/contests/abc002/submissions/2860675
+// Date: Tue, 17 Jul 2018 13:21:58 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+#include <assert.h>
+
+using namespace std;
+
+int main()
+{
+	int x, y;
+	cin >> x >> y;
+	cout << max(x, y) << endl;
+
+	return 0;
+}
