9614121d6e863c23ab3b68cb1b546b31379dfa65
ecto0310 <ecto0310@gmail.com>
2018-07-17 12:52:00 +0000

    Add atcoder/abc021/abc021_a/2860528.cpp

---
+++ atcoder/abc021/abc021_a/2860528.cpp
@@ -0,0 +1,18 @@
+// URL: https://atcoder.jp/contests/abc021/submissions/2860528
+// Date: Tue, 17 Jul 2018 12:52:00 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+#include <assert.h>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	cout << n << endl;
+	for (int i = 0; i < n; i++)
+		cout << "1" << endl;
+
+	return 0;
+}
