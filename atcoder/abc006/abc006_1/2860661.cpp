0ef70ba5f89534aa17236ea97cbd97fb0fbcad21
ecto0310 <ecto0310@gmail.com>
2018-07-17 13:17:30 +0000

    Add atcoder/abc006/abc006_1/2860661.cpp

---
+++ atcoder/abc006/abc006_1/2860661.cpp
@@ -0,0 +1,19 @@
+// URL: https://atcoder.jp/contests/abc006/submissions/2860661
+// Date: Tue, 17 Jul 2018 13:17:30 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+#include <assert.h>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	if (n % 3 == 0)
+		cout << "YES" << endl;
+	else
+		cout << "NO" << endl;
+
+	return 0;
+}
