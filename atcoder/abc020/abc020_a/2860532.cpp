48f78e3ba5418d65695c0821d815182531986a51
ecto0310 <ecto0310@gmail.com>
2018-07-17 12:53:02 +0000

    Add atcoder/abc020/abc020_a/2860532.cpp

---
+++ atcoder/abc020/abc020_a/2860532.cpp
@@ -0,0 +1,19 @@
+// URL: https://atcoder.jp/contests/abc020/submissions/2860532
+// Date: Tue, 17 Jul 2018 12:53:02 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+#include <assert.h>
+
+using namespace std;
+
+int main()
+{
+	int q;
+	cin >> q;
+	if (q == 1)
+		cout << "ABC" << endl;
+	else
+		cout << "chokudai" << endl;
+
+	return 0;
+}
