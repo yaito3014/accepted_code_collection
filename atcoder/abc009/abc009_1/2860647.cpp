4f649d0e651f536ee244d429e20ede628d9a2568
ecto0310 <ecto0310@gmail.com>
2018-07-17 13:14:55 +0000

    Add atcoder/abc009/abc009_1/2860647.cpp

---
+++ atcoder/abc009/abc009_1/2860647.cpp
@@ -0,0 +1,16 @@
+// URL: https://atcoder.jp/contests/abc009/submissions/2860647
+// Date: Tue, 17 Jul 2018 13:14:55 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+#include <assert.h>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	cout << (n + 1) / 2 << endl;
+
+	return 0;
+}
