887212c7b9729155e7f7aaabf0afa51451014761
ecto0310 <ecto0310@gmail.com>
2018-07-17 13:11:54 +0000

    Add atcoder/abc013/abc013_1/2860629.cpp

---
+++ atcoder/abc013/abc013_1/2860629.cpp
@@ -0,0 +1,16 @@
+// URL: https://atcoder.jp/contests/abc013/submissions/2860629
+// Date: Tue, 17 Jul 2018 13:11:54 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+#include <assert.h>
+
+using namespace std;
+
+int main()
+{
+	string a;
+	cin >> a;
+	cout << 1 + a[0] - 'A' << endl;
+
+	return 0;
+}
