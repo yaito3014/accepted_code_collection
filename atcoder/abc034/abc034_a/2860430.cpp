64273179fb477dbf97c0abfd81e9d79ecd0ef217
ecto0310 <ecto0310@gmail.com>
2018-07-17 12:30:27 +0000

    Add atcoder/abc034/abc034_a/2860430.cpp

---
+++ atcoder/abc034/abc034_a/2860430.cpp
@@ -0,0 +1,19 @@
+// URL: https://atcoder.jp/contests/abc034/submissions/2860430
+// Date: Tue, 17 Jul 2018 12:30:27 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+#include <assert.h>
+
+using namespace std;
+
+int main()
+{
+	int x, y;
+	cin >> x >> y;
+	if (x > y)
+		cout << "Worse" << endl;
+	else
+		cout << "Better" << endl;
+
+	return 0;
+}
