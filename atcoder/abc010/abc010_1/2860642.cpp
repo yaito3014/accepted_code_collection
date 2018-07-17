78d8c68a12b6b5f8c58af88032b226170274a234
ecto0310 <ecto0310@gmail.com>
2018-07-17 13:14:15 +0000

    Add atcoder/abc010/abc010_1/2860642.cpp

---
+++ atcoder/abc010/abc010_1/2860642.cpp
@@ -0,0 +1,16 @@
+// URL: https://atcoder.jp/contests/abc010/submissions/2860642
+// Date: Tue, 17 Jul 2018 13:14:15 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+#include <assert.h>
+
+using namespace std;
+
+int main()
+{
+	string s;
+	cin >> s;
+	cout << s << "pp" << endl;
+
+	return 0;
+}
