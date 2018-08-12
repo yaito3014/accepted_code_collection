64387f7b30bab0994062c99419c2e60da1a33284
ecto0310 <ecto0310@gmail.com>
2018-08-12 09:14:30 +0000

    Add atcoder/abc105/abc105_a/2998057.cpp

---
+++ atcoder/abc105/abc105_a/2998057.cpp
@@ -0,0 +1,16 @@
+// URL: https://atcoder.jp/contests/abc105/submissions/2998057
+// Date: Sun, 12 Aug 2018 09:14:30 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+#include <assert.h>
+
+using namespace std;
+
+int main()
+{
+	int n, k;
+	cin >> n >> k;
+	cout << (n % k == 0 ? 0 : 1) << endl;
+
+	return 0;
+}
