c6de73f9081531ac78a79a9a711d64357c54a6b3
yaito3014 <ykakeyama3014@gmail.com>
2018-08-18 12:48:46 +0000

    Add atcoder/abc106/abc106_a/3035687.cpp

---
+++ atcoder/abc106/abc106_a/3035687.cpp
@@ -0,0 +1,11 @@
+// URL: https://atcoder.jp/contests/abc106/submissions/3035687
+// Date: Sat, 18 Aug 2018 12:48:46 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <iostream>
+
+int main() {
+	int A, B;
+	std::cin >> A >> B;
+	std::cout << (A - 1)*(B - 1) << std::endl;
+	return 0;
+}
