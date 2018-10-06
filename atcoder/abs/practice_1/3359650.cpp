81b9249627928a99428a9492433bc282f1ae275a
yaito3014 <ykakeyama3014@gmail.com>
2018-10-06 23:36:13 +0000

    Add atcoder/abs/practice_1/3359650.cpp

---
+++ atcoder/abs/practice_1/3359650.cpp
@@ -0,0 +1,14 @@
+// URL: https://atcoder.jp/contests/abs/submissions/3359650
+// Date: Sat, 06 Oct 2018 23:36:13 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <iostream>
+#include <string>
+
+int main() {
+
+	int a, b, c;
+	std::string s;
+	std::cin >> a >> b >> c >> s;
+	printf("%d %s\n", a + b + c, s.c_str());
+	return 0;
+}
