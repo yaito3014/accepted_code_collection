dfe28a7cc1224879ed2f1b07ec19d4c2c6282f9c
yaito3014 <ykakeyama3014@gmail.com>
2018-10-06 12:45:42 +0000

    Add atcoder/abc112/abc112_a/3349518.cpp

---
+++ atcoder/abc112/abc112_a/3349518.cpp
@@ -0,0 +1,17 @@
+// URL: https://atcoder.jp/contests/abc112/submissions/3349518
+// Date: Sat, 06 Oct 2018 12:45:42 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <cstdio>
+
+int main() {
+	int N, A, B;
+	scanf("%d", &N);
+	if (N - 1) {
+		scanf("%d", &A);
+		scanf("%d", &B);
+		printf("%d", A + B);
+	}
+	else {
+		puts("Hello World");
+	}
+}
