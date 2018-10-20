f83c6cfc59e758bdf396279981c9931477239e36
yaito3014 <ykakeyama3014@gmail.com>
2018-10-20 04:35:24 +0000

    Add atcoder/qupc2018/qupc2018_a/3436328.cpp

---
+++ atcoder/qupc2018/qupc2018_a/3436328.cpp
@@ -0,0 +1,10 @@
+// URL: https://atcoder.jp/contests/qupc2018/submissions/3436328
+// Date: Sat, 20 Oct 2018 04:35:24 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <iostream>
+
+int main(){
+ int N;
+  std::cin >> N;
+  printf("%d", 2014+4*(N-1));
+}
