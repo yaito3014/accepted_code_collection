e28e356174623cafb843975fb0c3e7a77e817082
ecto0310 <ecto0310@gmail.com>
2018-11-04 12:00:45 +0000

    Add atcoder/abc113/abc113_a/3530929.cpp

---
+++ atcoder/abc113/abc113_a/3530929.cpp
@@ -0,0 +1,16 @@
+// URL: https://atcoder.jp/contests/abc113/submissions/3530929
+// Date: Sun, 04 Nov 2018 12:00:45 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+#include <assert.h>
+
+using namespace std;
+
+int main()
+{
+	int x, y;
+	cin >> x >> y;
+	cout << x + y / 2 << endl;
+
+	return 0;
+}
