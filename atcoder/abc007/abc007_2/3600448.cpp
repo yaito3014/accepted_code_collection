3ae8d9ee50e3c7117a01859dee4d7041fc9327a1
ecto0310 <ecto0310@gmail.com>
2018-11-14 14:50:08 +0000

    Add atcoder/abc007/abc007_2/3600448.cpp

---
+++ atcoder/abc007/abc007_2/3600448.cpp
@@ -0,0 +1,16 @@
+// URL: https://atcoder.jp/contests/abc007/submissions/3600448
+// Date: Wed, 14 Nov 2018 14:50:08 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+#include <assert.h>
+
+using namespace std;
+
+int main()
+{
+	string a;
+	cin >> a;
+	cout << (a == "a" ? "-1" : "a") << endl;
+
+	return 0;
+}
