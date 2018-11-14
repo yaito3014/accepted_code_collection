76837d19a6297d1613bb13aafab9611903488ebe
ecto0310 <ecto0310@gmail.com>
2018-11-14 13:53:49 +0000

    Add atcoder/abc020/abc020_b/3600170.cpp

---
+++ atcoder/abc020/abc020_b/3600170.cpp
@@ -0,0 +1,16 @@
+// URL: https://atcoder.jp/contests/abc020/submissions/3600170
+// Date: Wed, 14 Nov 2018 13:53:49 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+#include <assert.h>
+
+using namespace std;
+
+int main()
+{
+	string a, b;
+	cin >> a >> b;
+	cout << stoi(a + b) * 2 << endl;
+
+	return 0;
+}
