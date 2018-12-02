e90f2e28932c6605e3dd5d6eb335efa5dbcbac45
ecto0310 <ecto0310@gmail.com>
2018-12-02 12:00:43 +0000

    Add atcoder/abc114/abc114_a/3700827.cpp

---
+++ atcoder/abc114/abc114_a/3700827.cpp
@@ -0,0 +1,16 @@
+// URL: https://atcoder.jp/contests/abc114/submissions/3700827
+// Date: Sun, 02 Dec 2018 12:00:43 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+#include <assert.h>
+
+using namespace std;
+
+int main()
+{
+	int x;
+	cin >> x;
+	cout << (x == 3 || x == 5 || x == 7 ? "YES" : "NO") << endl;
+
+	return 0;
+}
