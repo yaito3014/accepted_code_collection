25a2977b2d4c72d49f2437174821cc3f0e661446
yaito3014 <ykakeyama3014@gmail.com>
2018-12-08 12:22:33 +0000

    Add atcoder/abc115/abc115_a/3742505.cpp

---
+++ atcoder/abc115/abc115_a/3742505.cpp
@@ -0,0 +1,24 @@
+// URL: https://atcoder.jp/contests/abc115/submissions/3742505
+// Date: Sat, 08 Dec 2018 12:22:33 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <iostream>
+#include <string>
+
+int main(){
+ int D;
+  std::cin >> D;
+  switch(D){
+    case 22:
+      std::cout << "Christmas Eve Eve Eve" << std::endl;
+      break;
+      case 23:
+      std::cout << "Christmas Eve Eve" << std::endl;
+      break;
+      case 24:
+      std::cout << "Christmas Eve" << std::endl;
+      break;
+      case 25:
+      std::cout << "Christmas" << std::endl;
+      break;
+  }
+}
