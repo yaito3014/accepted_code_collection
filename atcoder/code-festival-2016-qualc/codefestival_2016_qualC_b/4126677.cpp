07a78ed9bb9474f960742e036e26272e6ac45ee2
ecto0310 <ecto0310@gmail.com>
2019-01-30 07:44:03 +0000

    Add atcoder/code-festival-2016-qualc/codefestival_2016_qualC_b/4126677.cpp

---
+++ atcoder/code-festival-2016-qualc/codefestival_2016_qualC_b/4126677.cpp
@@ -0,0 +1,19 @@
+// URL: https://atcoder.jp/contests/code-festival-2016-qualc/submissions/4126677
+// Date: Wed, 30 Jan 2019 07:44:03 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+#include <assert.h>
+
+using namespace std;
+
+int main()
+{
+  int K, T;
+  cin >> K >> T;
+  vector<int> a(T);
+  for (int i = 0; i < T; i++)
+    cin >> a[i];
+  cout << max(0, *max_element(a.begin(), a.end()) * 2 - K - 1) << endl;
+
+  return 0;
+}
