6f20b6ebd648ae6d8275600145ef6b44c56a72b0
yaito3014 <ykakeyama3014@gmail.com>
2019-02-03 12:58:21 +0000

    Add atcoder/abc117/abc117_a/4162325.cpp

---
+++ atcoder/abc117/abc117_a/4162325.cpp
@@ -0,0 +1,10 @@
+// URL: https://atcoder.jp/contests/abc117/submissions/4162325
+// Date: Sun, 03 Feb 2019 12:58:21 +0000
+// Language: C++14 (GCC 5.4.1)
+
+#include <iostream>
+int main() {
+	int T, X;
+	std::cin >> T >> X;
+	std::cout << static_cast<float>(T)/X << std::endl;
+}
