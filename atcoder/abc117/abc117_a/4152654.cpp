97d524d4feb8be1048ddc17c968aefa3566c8fb3
ecto0310 <ecto0310@gmail.com>
2019-02-03 12:00:42 +0000

    Add atcoder/abc117/abc117_a/4152654.cpp

---
+++ atcoder/abc117/abc117_a/4152654.cpp
@@ -0,0 +1,16 @@
+// URL: https://atcoder.jp/contests/abc117/submissions/4152654
+// Date: Sun, 03 Feb 2019 12:00:42 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+#include <assert.h>
+
+using namespace std;
+
+int main()
+{
+  int T, X;
+  cin >> T >> X;
+  cout << (double)T / X << endl;
+
+  return 0;
+}
