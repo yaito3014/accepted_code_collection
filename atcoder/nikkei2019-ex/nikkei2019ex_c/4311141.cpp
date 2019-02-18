780bd43b2878a6bb4192fea7f02916908f9f4534
ecto0310 <ecto0310@gmail.com>
2019-02-18 07:13:06 +0000

    Add atcoder/nikkei2019-ex/nikkei2019ex_c/4311141.cpp

---
+++ atcoder/nikkei2019-ex/nikkei2019ex_c/4311141.cpp
@@ -0,0 +1,16 @@
+// URL: https://atcoder.jp/contests/nikkei2019-ex/submissions/4311141
+// Date: Mon, 18 Feb 2019 07:13:06 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+#include <assert.h>
+
+using namespace std;
+
+int main()
+{
+  int N;
+  cin >> N;
+  cout << N % 11 << endl;
+
+  return 0;
+}
