11c87632f740c8a3cf1007f0198b7326938a03c7
yaito3014 <ykakeyama3014@gmail.com>
2019-03-03 13:53:32 +0000

    Add atcoder/abc120/abc120_a/4459458.cpp

---
+++ atcoder/abc120/abc120_a/4459458.cpp
@@ -0,0 +1,9 @@
+// URL: https://atcoder.jp/contests/abc120/submissions/4459458
+// Date: Sun, 03 Mar 2019 13:53:32 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <iostream>
+#include <algorithm>
+int main(){
+    int A,B,C;std::cin>>A>>B>>C;
+    std::cout<<std::min(B/A,C)<<std::endl;
+}
