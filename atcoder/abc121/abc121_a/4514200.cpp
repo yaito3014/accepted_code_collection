77033c92d8c432951fd22ce27d48ed9730cc2e1d
ecto0310 <ecto0310@gmail.com>
2019-03-09 12:01:20 +0000

    Add atcoder/abc121/abc121_a/4514200.cpp

---
+++ atcoder/abc121/abc121_a/4514200.cpp
@@ -0,0 +1,16 @@
+// URL: https://atcoder.jp/contests/abc121/submissions/4514200
+// Date: Sat, 09 Mar 2019 12:01:20 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+#include <assert.h>
+
+using namespace std;
+
+int main()
+{
+  int H, W, h, w;
+  cin >> H >> W >> h >> w;
+  cout << (H - h) * (W - w) << endl;
+
+  return 0;
+}
