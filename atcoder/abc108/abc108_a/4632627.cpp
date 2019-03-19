699ebfba233fb1c422ae22abc7a27cd43fb4a3b4
ecto0310 <ecto0310@gmail.com>
2019-03-19 11:31:58 +0000

    Add atcoder/abc108/abc108_a/4632627.cpp

---
+++ atcoder/abc108/abc108_a/4632627.cpp
@@ -0,0 +1,16 @@
+// URL: https://atcoder.jp/contests/abc108/submissions/4632627
+// Date: Tue, 19 Mar 2019 11:31:58 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+#include <assert.h>
+
+using namespace std;
+
+int main()
+{
+  int k;
+  cin >> k;
+  cout << k / 2 * ((k + 1) / 2) << endl;
+
+  return 0;
+}
