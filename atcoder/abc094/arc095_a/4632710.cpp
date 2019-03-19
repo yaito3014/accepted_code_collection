7379bf26df8a89d899f2d22931bfbcc4b33ab187
ecto0310 <ecto0310@gmail.com>
2019-03-19 11:38:04 +0000

    Add atcoder/abc094/arc095_a/4632710.cpp

---
+++ atcoder/abc094/arc095_a/4632710.cpp
@@ -0,0 +1,27 @@
+// URL: https://atcoder.jp/contests/abc094/submissions/4632710
+// Date: Tue, 19 Mar 2019 11:38:04 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+#include <assert.h>
+
+using namespace std;
+
+const int MOD = 1e9 + 7;
+
+int main()
+{
+  int n;
+  cin >> n;
+  vector<int> x(n);
+  for (int i = 0; i < n; i++)
+    cin >> x[i];
+  vector<int> s = x;
+  sort(s.begin(), s.end());
+  for (int i = 0; i < n; i++)
+    if (x[i] < s[(n + 1) / 2])
+      cout << s[(n + 1) / 2] << endl;
+    else
+      cout << s[(n + 1) / 2 - 1] << endl;
+
+  return 0;
+}
