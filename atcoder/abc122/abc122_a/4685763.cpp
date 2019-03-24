023db6c798a96bf1b72747a17b35e4de5d21a8ad
ecto0310 <ecto0310@gmail.com>
2019-03-24 12:01:55 +0000

    Add atcoder/abc122/abc122_a/4685763.cpp

---
+++ atcoder/abc122/abc122_a/4685763.cpp
@@ -0,0 +1,23 @@
+// URL: https://atcoder.jp/contests/abc122/submissions/4685763
+// Date: Sun, 24 Mar 2019 12:01:55 +0000
+// Language: C++14 (GCC 5.4.1)
+#include<bits/stdc++.h>
+#include<assert.h>
+
+using namespace std;
+
+int main()
+{
+	string b;
+	cin>>b;
+	if(b=="A")
+		cout<< "T"<< endl;
+		else if(b=="T")
+			cout<<"A"<< endl;
+			else if(b=="C")
+				cout<< "G"<< endl;
+				else
+					cout<< "C"<< endl;
+
+  return 0;
+}
