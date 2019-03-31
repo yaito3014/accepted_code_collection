92b9eb2968266b64e186815723efaf101930bc8a
ecto0310 <ecto0310@gmail.com>
2019-03-31 05:59:20 +0000

    Add atcoder/exawizards2019/exawizards2019_a/4783055.cpp

---
+++ atcoder/exawizards2019/exawizards2019_a/4783055.cpp
@@ -0,0 +1,19 @@
+// URL: https://atcoder.jp/contests/exawizards2019/submissions/4783055
+// Date: Sun, 31 Mar 2019 05:59:20 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+#include <assert.h>
+
+using namespace std;
+
+int main()
+{
+  int a, b, c;
+  cin >> a >> b >> c;
+  if (a == b && b == c)
+    cout << "Yes" << endl;
+  else
+    cout << "No" << endl;
+
+  return 0;
+}
