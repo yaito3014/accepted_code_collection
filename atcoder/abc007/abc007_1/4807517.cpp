568ff5dcca7159987ec1dd6ad584f1884ce0c4a2
yaito3014 <ykakeyama3014@gmail.com>
2019-04-02 14:21:58 +0000

    Add atcoder/abc007/abc007_1/4807517.cpp

---
+++ atcoder/abc007/abc007_1/4807517.cpp
@@ -0,0 +1,10 @@
+// URL: https://atcoder.jp/contests/abc007/submissions/4807517
+// Date: Tue, 02 Apr 2019 14:21:58 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+
+int main() {
+    int n;
+    std::cin >> n;
+    std::cout << n - 1 << std::endl;
+}
