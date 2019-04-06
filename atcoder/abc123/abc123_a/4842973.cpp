5bc179d606d470fedce01ba15fafe60ab59126b4
ecto0310 <ecto0310@gmail.com>
2019-04-06 12:01:55 +0000

    Add atcoder/abc123/abc123_a/4842973.cpp

---
+++ atcoder/abc123/abc123_a/4842973.cpp
@@ -0,0 +1,19 @@
+// URL: https://atcoder.jp/contests/abc123/submissions/4842973
+// Date: Sat, 06 Apr 2019 12:01:55 +0000
+// Language: C++14 (GCC 5.4.1)
+#include<bits/stdc++.h>
+#include<assert.h>
+
+using namespace std;
+
+int main()
+{
+	int a,b,c,d,e,k;
+	cin>>a>>b>>c>>d>>e>>k;
+	if(k<e-a)
+		cout<<":("<<endl;
+	else
+		cout<<"Yay!"<<endl;
+
+	return 0;
+}
