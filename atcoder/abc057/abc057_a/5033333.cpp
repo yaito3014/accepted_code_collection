9940f58b2d32cf06af8c3c1fe2ef174975482563
yaito3014 <ykakeyama3014@gmail.com>
2019-04-20 07:44:41 +0000

    Add atcoder/abc057/abc057_a/5033333.cpp

---
+++ atcoder/abc057/abc057_a/5033333.cpp
@@ -0,0 +1,15 @@
+// URL: https://atcoder.jp/contests/abc057/submissions/5033333
+// Date: Sat, 20 Apr 2019 07:44:41 +0000
+// Language: C++14 (GCC 5.4.1)
+#ifdef __GNUC__
+#include <bits/stdc++.h>
+#endif // __GNUC__
+
+int main() {
+    std::cin.tie(nullptr); std::ios::sync_with_stdio(false);
+
+    int A, B;
+    std::cin >> A >> B;
+    std::cout << (A + B) % 24 << std::endl;
+
+}
