d7594494538f9d945f1b87653675bc36556ee450
yaito3014 <ykakeyama3014@gmail.com>
2019-04-21 13:22:54 +0000

    Add atcoder/abc002/abc002_1/5078717.cpp

---
+++ atcoder/abc002/abc002_1/5078717.cpp
@@ -0,0 +1,12 @@
+// URL: https://atcoder.jp/contests/abc002/submissions/5078717
+// Date: Sun, 21 Apr 2019 13:22:54 +0000
+// Language: C++14 (GCC 5.4.1)
+#ifdef __GNUC__
+#include <bits/stdc++.h>
+#endif // __GNUC__
+
+int main() {
+    int X, Y;
+    std::cin >> X >> Y;
+    std::cout << std::max(X, Y) << std::endl;
+}
