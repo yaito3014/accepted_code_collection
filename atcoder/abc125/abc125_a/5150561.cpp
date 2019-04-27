1983a69f6fb33b7bd6201c01633d43bd9ee8a73c
yaito3014 <ykakeyama3014@gmail.com>
2019-04-27 12:35:00 +0000

    Add atcoder/abc125/abc125_a/5150561.cpp

---
+++ atcoder/abc125/abc125_a/5150561.cpp
@@ -0,0 +1,12 @@
+// URL: https://atcoder.jp/contests/abc125/submissions/5150561
+// Date: Sat, 27 Apr 2019 12:35:00 +0000
+// Language: C++14 (GCC 5.4.1)
+#ifdef __GNUC__
+#include <bits/stdc++.h>
+#endif // __GNUC_
+
+int main() {
+    int A, B, T;
+    std::cin >> A >> B >> T;
+    std::cout << (T / A ) * B << std::endl;
+}
