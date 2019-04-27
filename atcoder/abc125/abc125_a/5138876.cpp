bd73078959e0048fa97d917b6da2a056d755cae6
ecto0310 <ecto0310@gmail.com>
2019-04-27 12:01:31 +0000

    Add atcoder/abc125/abc125_a/5138876.cpp

---
+++ atcoder/abc125/abc125_a/5138876.cpp
@@ -0,0 +1,15 @@
+// URL: https://atcoder.jp/contests/abc125/submissions/5138876
+// Date: Sat, 27 Apr 2019 12:01:31 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+#include <assert.h>
+using namespace std;
+using i64 = long long;
+
+int main()
+{
+  i64 a, b, t;
+  cin >> a >> b >> t;
+  cout << t / a * b << endl;
+  return 0;
+}
