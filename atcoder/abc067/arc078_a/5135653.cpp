52fe773f169b73d2c32a10bdf150904731443a14
yaito3014 <ykakeyama3014@gmail.com>
2019-04-27 07:52:38 +0000

    Add atcoder/abc067/arc078_a/5135653.cpp

---
+++ atcoder/abc067/arc078_a/5135653.cpp
@@ -0,0 +1,24 @@
+// URL: https://atcoder.jp/contests/abc067/submissions/5135653
+// Date: Sat, 27 Apr 2019 07:52:38 +0000
+// Language: C++14 (GCC 5.4.1)
+#ifdef __GNUC__
+#include <bits/stdc++.h>
+#endif // __GNUC_
+
+using namespace std;
+
+int main() {
+    long long int N, b = 0, a[100000 * 2] = {}, ans = 0, sum[100000 * 2 + 1] = {}, dif = 0, mini = 1000000000 * 2 + 1;
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        cin >> a[i];
+        sum[i + 1] = sum[i] + a[i];
+    }
+    for (int i = 0; i < N - 1; i++) {
+        b = (sum[N] - sum[i + 1]) - sum[i + 1];
+        dif = abs(b);
+        mini = min(dif, mini);
+    }
+    cout << mini << endl;
+    return 0;
+}
