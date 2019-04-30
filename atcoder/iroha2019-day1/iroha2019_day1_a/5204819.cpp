0d19321337529cddd8d78906dda50dfeba14b566
yaito3014 <ykakeyama3014@gmail.com>
2019-04-30 10:45:50 +0000

    Add atcoder/iroha2019-day1/iroha2019_day1_a/5204819.cpp

---
+++ atcoder/iroha2019-day1/iroha2019_day1_a/5204819.cpp
@@ -0,0 +1,14 @@
+// URL: https://atcoder.jp/contests/iroha2019-day1/submissions/5204819
+// Date: Tue, 30 Apr 2019 10:45:50 +0000
+// Language: C++14 (GCC 5.4.1)
+#ifdef __GNUC__
+#include <bits/stdc++.h>
+#endif // __GNUC_
+
+int main() {
+
+    std::string S;
+    std::cin >> S;
+    std::cout << S[0] << std::endl;
+
+}
