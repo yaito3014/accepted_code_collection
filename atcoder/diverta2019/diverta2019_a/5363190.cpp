624bf57d6e77d312edc5d3219d4cc55762ef625b
yaito3014 <ykakeyama3014@gmail.com>
2019-05-11 13:46:08 +0000

    Add atcoder/diverta2019/diverta2019_a/5363190.cpp

---
+++ atcoder/diverta2019/diverta2019_a/5363190.cpp
@@ -0,0 +1,15 @@
+// URL: https://atcoder.jp/contests/diverta2019/submissions/5363190
+// Date: Sat, 11 May 2019 13:46:08 +0000
+// Language: C++14 (GCC 5.4.1)
+#ifdef __GNUC__
+#include <bits/stdc++.h>
+#endif // __GNUC_
+
+
+int main() {
+    
+    int N, K;
+    std::cin >> N >> K;
+    std::cout << N-K+1 << std::endl;
+    
+}
