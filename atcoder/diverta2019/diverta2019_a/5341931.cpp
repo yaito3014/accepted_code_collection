5038e6f0cc7ced73e8273c1e26782c80803b91ff
ecto0310 <ecto0310@gmail.com>
2019-05-11 12:16:10 +0000

    Add atcoder/diverta2019/diverta2019_a/5341931.cpp

---
+++ atcoder/diverta2019/diverta2019_a/5341931.cpp
@@ -0,0 +1,15 @@
+// URL: https://atcoder.jp/contests/diverta2019/submissions/5341931
+// Date: Sat, 11 May 2019 12:16:10 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+#include <assert.h>
+using namespace std;
+using i64 = long long;
+
+int main()
+{
+  i64 n, k;
+  cin >> n >> k;
+  cout << n - k + 1 << endl;
+  return 0;
+}
