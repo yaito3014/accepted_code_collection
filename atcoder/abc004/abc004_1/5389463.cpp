4c85cb2a2c326be7f3daccb75f25e3dcb31994cf
yaito3014 <ykakeyama3014@gmail.com>
2019-05-13 11:23:14 +0000

    Add atcoder/abc004/abc004_1/5389463.cpp

---
+++ atcoder/abc004/abc004_1/5389463.cpp
@@ -0,0 +1,11 @@
+// URL: https://atcoder.jp/contests/abc004/submissions/5389463
+// Date: Mon, 13 May 2019 11:23:14 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+int main() {
+	
+	int N;
+	std::cin >> N;
+	std::cout << N*2 << std::endl;
+	
+}
