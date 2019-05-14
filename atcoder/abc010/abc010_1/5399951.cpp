b67be76b66c3dbfbfc1c6b1dd0904d3de645a421
yaito3014 <ykakeyama3014@gmail.com>
2019-05-14 12:54:48 +0000

    Add atcoder/abc010/abc010_1/5399951.cpp

---
+++ atcoder/abc010/abc010_1/5399951.cpp
@@ -0,0 +1,15 @@
+// URL: https://atcoder.jp/contests/abc010/submissions/5399951
+// Date: Tue, 14 May 2019 12:54:48 +0000
+// Language: C++14 (GCC 5.4.1)
+#ifndef YK_INCLUDE
+#include <bits/stdc++.h>
+#endif //! YK_INCLUDE
+int main() {
+    std::cin.tie(nullptr); std::ios::sync_with_stdio(false);
+    
+    std::string S;
+    std::cin >> S;
+    std::cout << S << "pp
+";
+    
+}
