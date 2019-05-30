030e22b258aab67126c6bb1c2b64d699ab19b0fb
yaito3014 <ykakeyama3014@gmail.com>
2019-05-30 21:39:12 +0000

    Add atcoder/abc020/abc020_a/5706767.cpp

---
+++ atcoder/abc020/abc020_a/5706767.cpp
@@ -0,0 +1,14 @@
+// URL: https://atcoder.jp/contests/abc020/submissions/5706767
+// Date: Thu, 30 May 2019 21:39:12 +0000
+// Language: C++14 (GCC 5.4.1)
+#ifndef YK_INCLUDE
+#include <bits/stdc++.h>
+#endif //! YK_INCLUDE
+
+int main() {
+	
+	int Q;
+	std::cin>>Q;
+	std::cout<<(Q==1?"ABC":"chokudai")<<"\n";
+	
+}
