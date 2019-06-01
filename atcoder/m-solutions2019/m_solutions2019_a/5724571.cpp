375c6890be08f89b2543553df0f307714983849f
yaito3014 <ykakeyama3014@gmail.com>
2019-06-01 12:01:36 +0000

    Add atcoder/m-solutions2019/m_solutions2019_a/5724571.cpp

---
+++ atcoder/m-solutions2019/m_solutions2019_a/5724571.cpp
@@ -0,0 +1,12 @@
+// URL: https://atcoder.jp/contests/m-solutions2019/submissions/5724571
+// Date: Sat, 01 Jun 2019 12:01:36 +0000
+// Language: C++14 (GCC 5.4.1)
+#include "bits/stdc++.h"
+
+int main() {
+
+    int N;
+    std::cin >> N;
+    std::cout << (N - 2) * 180 << std::endl;
+
+}
