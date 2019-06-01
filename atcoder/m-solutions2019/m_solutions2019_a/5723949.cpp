060aab669e3d05887b5a397d5500810d4718ee59
ecto0310 <ecto0310@gmail.com>
2019-06-01 12:00:52 +0000

    Add atcoder/m-solutions2019/m_solutions2019_a/5723949.cpp

---
+++ atcoder/m-solutions2019/m_solutions2019_a/5723949.cpp
@@ -0,0 +1,15 @@
+// URL: https://atcoder.jp/contests/m-solutions2019/submissions/5723949
+// Date: Sat, 01 Jun 2019 12:00:52 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+#include <assert.h>
+using namespace std;
+using i64 = long long;
+
+int main()
+{
+  i64 n;
+  cin >> n;
+  cout << (n - 2) * 180 << endl;
+  return 0;
+}
