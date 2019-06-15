aed3402ebc98ed1107dbfcfa62c4ba61a28cd5d6
yaito3014 <ykakeyama3014@gmail.com>
2019-06-15 12:03:02 +0000

    Add atcoder/diverta2019-2/diverta2019_2_a/5918918.cpp

---
+++ atcoder/diverta2019-2/diverta2019_2_a/5918918.cpp
@@ -0,0 +1,12 @@
+// URL: https://atcoder.jp/contests/diverta2019-2/submissions/5918918
+// Date: Sat, 15 Jun 2019 12:03:02 +0000
+// Language: C++14 (GCC 5.4.1)
+#include "bits/stdc++.h"
+
+int main() {
+
+    int N, K;
+    std::cin >> N >> K;
+    std::cout << (K==1?0:N - K) << std::endl;
+
+}
