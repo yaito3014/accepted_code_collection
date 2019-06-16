4ef0272292f90cca3d77a658193abddb8efffe9f
yaito3014 <ykakeyama3014@gmail.com>
2019-06-16 12:01:38 +0000

    Add atcoder/abc130/abc130_a/5952153.cpp

---
+++ atcoder/abc130/abc130_a/5952153.cpp
@@ -0,0 +1,12 @@
+// URL: https://atcoder.jp/contests/abc130/submissions/5952153
+// Date: Sun, 16 Jun 2019 12:01:38 +0000
+// Language: C++14 (GCC 5.4.1)
+#include "bits/stdc++.h"
+
+int main() {
+
+    int X, A;
+    std::cin >> X >> A;
+    std::cout << (X < A ? 0 : 10) << std::endl;
+
+}
