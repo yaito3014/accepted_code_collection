492d98941bc52fb6c4ef684d8f1ced565b7a5d69
ecto0310 <ecto0310@gmail.com>
2019-07-07 12:00:54 +0000

    Add atcoder/abc133/abc133_a/6265623.cpp

---
+++ atcoder/abc133/abc133_a/6265623.cpp
@@ -0,0 +1,15 @@
+// URL: https://atcoder.jp/contests/abc133/submissions/6265623
+// Date: Sun, 07 Jul 2019 12:00:54 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+#include <assert.h>
+using namespace std;
+using i64 = long long;
+
+int main()
+{
+  i64 n, a, b;
+  cin >> n >> a >> b;
+  cout << min(n * a, b) << endl;
+  return 0;
+}
