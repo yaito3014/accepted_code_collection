5720cc33ccef8eecbe4dc0f3e46053cce0f87183
ecto0310 <ecto0310@gmail.com>
2019-07-09 06:41:00 +0000

    Add aizu_online_judge/0317/3729538.cpp

---
+++ aizu_online_judge/0317/3729538.cpp
@@ -0,0 +1,15 @@
+// URL: http://judge.u-aizu.ac.jp/onlinejudge/review.jsp?rid=3729538
+// Date: Tue, 09 Jul 2019 06:41:00 +0000
+// Language: C++14
+#include <bits/stdc++.h>
+#include <assert.h>
+using namespace std;
+using i64 = long long;
+
+int main()
+{
+  i64 d, l;
+  cin >> d >> l;
+  cout << d / l + d % l << endl;
+  return 0;
+}
