8de08dd24cc91eb64885abd75136b14a9966cecc
ecto0310 <ecto0310@gmail.com>
2019-07-20 12:00:26 +0000

    Add atcoder/abc134/abc134_a/6445603.cpp

---
+++ atcoder/abc134/abc134_a/6445603.cpp
@@ -0,0 +1,15 @@
+// URL: https://atcoder.jp/contests/abc134/submissions/6445603
+// Date: Sat, 20 Jul 2019 12:00:26 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+#include <assert.h>
+using namespace std;
+using i64 = long long;
+
+int main()
+{
+  i64 r;
+  cin >> r;
+  cout << 3 * r * r << endl;
+  return 0;
+}
