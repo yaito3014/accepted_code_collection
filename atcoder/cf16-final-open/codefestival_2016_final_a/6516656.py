6d73a56baa5b0730e60ea6acd9ee1e2df607728b
yaito3014 <ykakeyama3014@gmail.com>
2019-07-23 11:04:13 +0000

    Add atcoder/cf16-final-open/codefestival_2016_final_a/6516656.py

---
+++ atcoder/cf16-final-open/codefestival_2016_final_a/6516656.py
@@ -0,0 +1,4 @@
+// URL: https://atcoder.jp/contests/cf16-final-open/submissions/6516656
+// Date: Tue, 23 Jul 2019 11:04:13 +0000
+// Language: Python3 (3.4.3)
+[(print(chr(ord('A')+i)+str(h+1)),quit())for H,W in [map(int,input().split())]for h in range(H)for i,v in enumerate(input().split())if v=="snuke"]
