445388a1dcf28dead489abb16a43e07f32b207ab
yaito3014 <ykakeyama3014@gmail.com>
2019-08-02 10:31:33 +0000

    Add atcoder/otemae2019/otemae2019_a/6648633.cpp

---
+++ atcoder/otemae2019/otemae2019_a/6648633.cpp
@@ -0,0 +1,12 @@
+// URL: https://atcoder.jp/contests/otemae2019/submissions/6648633
+// Date: Fri, 02 Aug 2019 10:31:33 +0000
+// Language: C++14 (GCC 5.4.1)
+// Copyright 2019 yaito3014
+#include "bits/stdc++.h"
+using namespace std;
+int main() {
+  float A, B;
+  cin >> A >> B;
+  bool ans = B < A + 0.5f;
+  cout << ans << endl;
+}
