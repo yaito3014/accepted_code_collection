96917828d0875316078b7e804d1e198c33a38e40
yaito3014 <ykakeyama3014@gmail.com>
2019-08-16 09:12:42 +0000

    Add aizu_online_judge/ITP1_1_A/3813476.cpp

---
+++ aizu_online_judge/ITP1_1_A/3813476.cpp
@@ -0,0 +1,8 @@
+// URL: http://judge.u-aizu.ac.jp/onlinejudge/review.jsp?rid=3813476
+// Date: Fri, 16 Aug 2019 09:12:42 +0000
+// Language: C++
+#include <iostream>
+
+int main(){
+    std::cout << "Hello World" << std::endl;
+}
