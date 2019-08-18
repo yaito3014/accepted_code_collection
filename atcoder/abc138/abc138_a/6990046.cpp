7e2b5b811b992d320c2fe1559061adb656d29871
yaito3014 <ykakeyama3014@gmail.com>
2019-08-18 12:11:32 +0000

    Add atcoder/abc138/abc138_a/6990046.cpp

---
+++ atcoder/abc138/abc138_a/6990046.cpp
@@ -0,0 +1,14 @@
+// URL: https://atcoder.jp/contests/abc138/submissions/6990046
+// Date: Sun, 18 Aug 2019 12:11:32 +0000
+// Language: C++14 (GCC 5.4.1)
+// Copyright 2019 yaito3014
+#include "bits/stdc++.h"
+using namespace std;
+
+int main() {
+  int a;
+  cin >> a;
+  string s;
+  cin >> s;
+  cout << (a < 3200 ? "red" : s) << endl;
+}
