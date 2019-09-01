17abd4f1180aac20f444e1609cd8a2f0f25f3fb4
ecto0310 <ecto0310@gmail.com>
2019-09-01 12:11:29 +0000

    Add atcoder/abc139/abc139_d/7256971.cpp

---
+++ atcoder/abc139/abc139_d/7256971.cpp
@@ -0,0 +1,15 @@
+// URL: https://atcoder.jp/contests/abc139/submissions/7256971
+// Date: Sun, 01 Sep 2019 12:11:29 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+#include <assert.h>
+using namespace std;
+using i64 = long long;
+
+int main()
+{
+  i64 n;
+  cin >> n;
+  cout << n * (n - 1) / 2 << endl;
+  return 0;
+}
