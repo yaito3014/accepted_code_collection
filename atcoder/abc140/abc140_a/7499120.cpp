7f81099dab4176cd0c1ace23d5ba52b28a3c58cf
yaito3014 <ykakeyama3014@gmail.com>
2019-09-14 09:26:07 +0000

    Add atcoder/abc140/abc140_a/7499120.cpp

---
+++ atcoder/abc140/abc140_a/7499120.cpp
@@ -0,0 +1,12 @@
+// URL: https://atcoder.jp/contests/abc140/submissions/7499120
+// Date: Sat, 14 Sep 2019 09:26:07 +0000
+// Language: C++14 (GCC 5.4.1)
+// Copyright 2019 yaito3014
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int N;
+	cin >> N;
+	cout << N*N*N << endl;
+}
