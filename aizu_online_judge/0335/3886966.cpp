15e94dac7e27908c11c551254b27d282c959507d
ecto0310 <ecto0310@gmail.com>
2019-09-22 15:35:21 +0000

    Add aizu_online_judge/0335/3886966.cpp

---
+++ aizu_online_judge/0335/3886966.cpp
@@ -0,0 +1,15 @@
+// URL: http://judge.u-aizu.ac.jp/onlinejudge/review.jsp?rid=3886966
+// Date: Sun, 22 Sep 2019 15:35:21 +0000
+// Language: C++14
+#include <bits/stdc++.h>
+#include <assert.h>
+using namespace std;
+using i64 = long long;
+
+int main()
+{
+  i64 w;
+  cin >> w;
+  cout << w * 32 << endl;
+  return 0;
+}
