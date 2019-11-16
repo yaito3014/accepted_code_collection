2dc316c1af23c5fa6b5c53055fee46b9b5bb816f
ecto0310 <ecto0310@gmail.com>
2019-11-16 12:00:27 +0000

    Add atcoder/abc145/abc145_a/8461970.cpp

---
+++ atcoder/abc145/abc145_a/8461970.cpp
@@ -0,0 +1,14 @@
+// URL: https://atcoder.jp/contests/abc145/submissions/8461970
+// Date: Sat, 16 Nov 2019 12:00:27 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+
+int main()
+{
+  i64 r;
+  cin >> r;
+  cout << r * r << endl;
+  return 0;
+}
