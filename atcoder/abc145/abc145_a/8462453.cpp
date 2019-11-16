cbb5312a8f37bcab5b029ca57a3b248f18f40aea
yaito3014 <ykakeyama3014@gmail.com>
2019-11-16 12:00:45 +0000

    Add atcoder/abc145/abc145_a/8462453.cpp

---
+++ atcoder/abc145/abc145_a/8462453.cpp
@@ -0,0 +1,14 @@
+// URL: https://atcoder.jp/contests/abc145/submissions/8462453
+// Date: Sat, 16 Nov 2019 12:00:45 +0000
+// Language: C++14 (GCC 5.4.1)
+// Copyright 2019 yaito3014
+#include "bits/stdc++.h"
+using namespace std;
+using namespace literals;
+
+int main() {
+  cin.tie(nullptr); ios::sync_with_stdio(false);
+  int r;
+  cin >> r;
+  cout << r * r << endl;
+}
