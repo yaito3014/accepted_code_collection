32a69b27f55be5b73aefe4043937a13f7e90bbc0
yaito3014 <ykakeyama3014@gmail.com>
2019-11-24 04:31:15 +0000

    Add atcoder/gigacode-2019/gigacode_2019_a/8595184.cpp

---
+++ atcoder/gigacode-2019/gigacode_2019_a/8595184.cpp
@@ -0,0 +1,13 @@
+// URL: https://atcoder.jp/contests/gigacode-2019/submissions/8595184
+// Date: Sun, 24 Nov 2019 04:31:15 +0000
+// Language: C++14 (GCC 5.4.1)
+#include "bits/stdc++.h"
+using namespace std;
+using namespace literals;
+
+int main() {
+  cin.tie(nullptr); ios::sync_with_stdio(false);
+  int A, B;
+  cin >> A >> B;
+  cout << A * B * B << endl;
+}
