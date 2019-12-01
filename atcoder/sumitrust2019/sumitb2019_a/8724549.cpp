4e603d4a6c9f7b8e6b8fc2386470d23ae2cb9f1f
yaito3014 <ykakeyama3014@gmail.com>
2019-12-01 12:02:09 +0000

    Add atcoder/sumitrust2019/sumitb2019_a/8724549.cpp

---
+++ atcoder/sumitrust2019/sumitb2019_a/8724549.cpp
@@ -0,0 +1,13 @@
+// URL: https://atcoder.jp/contests/sumitrust2019/submissions/8724549
+// Date: Sun, 01 Dec 2019 12:02:09 +0000
+// Language: C++14 (GCC 5.4.1)
+#include "bits/stdc++.h"
+using namespace std;
+using namespace literals;
+
+int main() {
+  cin.tie(nullptr); ios::sync_with_stdio(false);
+  int M1, D1, M2, D2;
+  cin >> M1 >> D1 >> M2 >> D2;
+  cout << (M1 + 1 == M2 ? 1 : 0) << endl;
+}
