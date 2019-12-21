ea59e9586f2d308a0e8ec5ca9ded7931599decf8
ecto0310 <ecto0310@gmail.com>
2019-12-21 11:07:42 +0000

    Add codeforces/1269/1269_a/67330270.cpp

---
+++ codeforces/1269/1269_a/67330270.cpp
@@ -0,0 +1,14 @@
+// URL: https://codeforces.com/contest/1269/submission/67330270
+// Date: Sat, 21 Dec 2019 11:07:42 +0000
+// Language: GNU C++17
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+
+int main()
+{
+  i64 n;
+  cin >> n;
+  cout << 16 * n << " " << 15 * n << endl;
+  return 0;
+}
