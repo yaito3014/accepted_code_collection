6c1fa64098ed41b5300f47bce253729be9df0784
ecto0310 <ecto0310@gmail.com>
2019-12-29 11:00:25 +0000

    Add atcoder/abc149/abc149_a/9205950.cpp

---
+++ atcoder/abc149/abc149_a/9205950.cpp
@@ -0,0 +1,14 @@
+// URL: https://atcoder.jp/contests/abc149/submissions/9205950
+// Date: Sun, 29 Dec 2019 11:00:25 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+
+int main()
+{
+  string s, t;
+  cin >> s >> t;
+  cout << t << s << endl;
+  return 0;
+}
