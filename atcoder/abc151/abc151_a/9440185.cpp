f087ab6782358ad5bf7493773011ca49c875c0d0
ecto0310 <ecto0310@gmail.com>
2020-01-12 12:00:35 +0000

    Add atcoder/abc151/abc151_a/9440185.cpp

---
+++ atcoder/abc151/abc151_a/9440185.cpp
@@ -0,0 +1,14 @@
+// URL: https://atcoder.jp/contests/abc151/submissions/9440185
+// Date: Sun, 12 Jan 2020 12:00:35 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+
+int main()
+{
+  string c;
+  cin >> c;
+  cout << (char)(c[0] + 1) << endl;
+  return 0;
+}
