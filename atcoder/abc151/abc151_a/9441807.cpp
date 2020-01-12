61e41b744d2b07dbc95ada9994affa3c0d2f940c
yaito3014 <ykakeyama3014@gmail.com>
2020-01-12 12:01:34 +0000

    Add atcoder/abc151/abc151_a/9441807.cpp

---
+++ atcoder/abc151/abc151_a/9441807.cpp
@@ -0,0 +1,14 @@
+// URL: https://atcoder.jp/contests/abc151/submissions/9441807
+// Date: Sun, 12 Jan 2020 12:01:34 +0000
+// Language: C++14 (GCC 5.4.1)
+// Copyright 2019 yaito3014
+#include "bits/stdc++.h"
+using namespace std;
+using namespace literals;
+
+int main() {
+  cin.tie(nullptr); ios::sync_with_stdio(false);
+  char C;
+  cin >> C;
+  cout << static_cast<char>(C + 1) << endl;
+}
