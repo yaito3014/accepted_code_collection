5b5819de5e080c48624dab3bb645cdcc87186d8e
ecto0310 <ecto0310@gmail.com>
2020-01-13 06:08:32 +0000

    Add atcoder/abc150/abc150_a/9491944.cpp

---
+++ atcoder/abc150/abc150_a/9491944.cpp
@@ -0,0 +1,17 @@
+// URL: https://atcoder.jp/contests/abc150/submissions/9491944
+// Date: Mon, 13 Jan 2020 06:08:32 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+
+int main()
+{
+  i64 k, x;
+  cin >> k >> x;
+  if (x <= k * 500)
+    cout << "Yes" << endl;
+  else
+    cout << "No" << endl;
+  return 0;
+}
