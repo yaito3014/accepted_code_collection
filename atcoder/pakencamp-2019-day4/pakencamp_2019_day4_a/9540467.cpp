9f763f49f8bf70a36a4a323fe6b3896e12667992
ecto0310 <ecto0310@gmail.com>
2020-01-16 21:52:38 +0000

    Add atcoder/pakencamp-2019-day4/pakencamp_2019_day4_a/9540467.cpp

---
+++ atcoder/pakencamp-2019-day4/pakencamp_2019_day4_a/9540467.cpp
@@ -0,0 +1,14 @@
+// URL: https://atcoder.jp/contests/pakencamp-2019-day4/submissions/9540467
+// Date: Thu, 16 Jan 2020 21:52:38 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+
+int main()
+{
+  i64 n;
+  cin >> n;
+  cout << (n + 399) / 400 << endl;
+  return 0;
+}
