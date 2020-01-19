315434807eb076d04c33a7e18aa5916616894b7b
ecto0310 <ecto0310@gmail.com>
2020-01-19 11:50:33 +0000

    Add atcoder/abc152/abc152_a/9596813.cpp

---
+++ atcoder/abc152/abc152_a/9596813.cpp
@@ -0,0 +1,17 @@
+// URL: https://atcoder.jp/contests/abc152/submissions/9596813
+// Date: Sun, 19 Jan 2020 11:50:33 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+
+int main()
+{
+  i64 n, m;
+  cin >> n >> m;
+  if (n == m)
+    cout << "Yes" << endl;
+  else
+    cout << "No" << endl;
+  return 0;
+}
