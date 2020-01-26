57e2d6bfc354bf02dcecfee12bc1c16a16c85a5e
ecto0310 <ecto0310@gmail.com>
2020-01-26 12:00:33 +0000

    Add atcoder/abc153/abc153_a/9729487.cpp

---
+++ atcoder/abc153/abc153_a/9729487.cpp
@@ -0,0 +1,14 @@
+// URL: https://atcoder.jp/contests/abc153/submissions/9729487
+// Date: Sun, 26 Jan 2020 12:00:33 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+
+int main()
+{
+  int h, a;
+  cin >> h >> a;
+  cout << (h + a - 1) / a << endl;
+  return 0;
+}
