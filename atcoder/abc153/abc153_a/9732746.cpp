6d7855cdf0ce1cf2efdb65146365ed772f2fa8a7
yaito3014 <ykakeyama3014@gmail.com>
2020-01-26 12:02:35 +0000

    Add atcoder/abc153/abc153_a/9732746.cpp

---
+++ atcoder/abc153/abc153_a/9732746.cpp
@@ -0,0 +1,13 @@
+// URL: https://atcoder.jp/contests/abc153/submissions/9732746
+// Date: Sun, 26 Jan 2020 12:02:35 +0000
+// Language: C++14 (GCC 5.4.1)
+// Copyright 2019 yaito3014
+#include "bits/stdc++.h"
+using namespace std;
+
+int main() {
+  cin.tie(nullptr); cout.tie(nullptr); ios::sync_with_stdio(false);
+  int H, A;
+  cin >> H >> A;
+  cout << (int)ceil(H / (float)A) << endl;
+}
