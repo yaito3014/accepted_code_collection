778af6362ff6210d930735946728d626d50a4c18
ecto0310 <ecto0310@gmail.com>
2020-02-14 06:45:38 +0000

    Add atcoder/abc154/abc154_b/10085597.cpp

---
+++ atcoder/abc154/abc154_b/10085597.cpp
@@ -0,0 +1,14 @@
+// URL: https://atcoder.jp/contests/abc154/submissions/10085597
+// Date: Fri, 14 Feb 2020 06:45:38 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+
+int main()
+{
+  string s;
+  cin >> s;
+  cout << string(s.size(), 'x') << endl;
+  return 0;
+}
