58ad8cada5dbea3580fb1b8de2444338c2acdcbd
yaito3014 <ykakeyama3014@gmail.com>
2020-02-26 22:30:52 +0000

    Add atcoder/abc109/abc109_a/10365968.cpp

---
+++ atcoder/abc109/abc109_a/10365968.cpp
@@ -0,0 +1,12 @@
+// URL: https://atcoder.jp/contests/abc109/submissions/10365968
+// Date: Wed, 26 Feb 2020 22:30:52 +0000
+// Language: C++14 (GCC 5.4.1)
+// Copyright 2020 yaito3014
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int A, B;
+	cin >> A >> B;
+	cout << (A*B%2?"Yes":"No") << endl;
+}
