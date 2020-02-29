d1f636d88e529df032197497ca4304285b8480b4
yaito3014 <ykakeyama3014@gmail.com>
2020-02-29 13:51:23 +0000

    Add atcoder/abc108/abc108_a/10408476.cpp

---
+++ atcoder/abc108/abc108_a/10408476.cpp
@@ -0,0 +1,12 @@
+// URL: https://atcoder.jp/contests/abc108/submissions/10408476
+// Date: Sat, 29 Feb 2020 13:51:23 +0000
+// Language: C++14 (GCC 5.4.1)
+// Copyright 2020 yaito3014
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int K;
+	cin >> K;
+	cout << (K/2)*(K/2+K%2) << endl;
+}
