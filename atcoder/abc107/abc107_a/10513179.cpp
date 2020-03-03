d7c87d5a4662746b468762d8862a693ebfcb21fe
yaito3014 <ykakeyama3014@gmail.com>
2020-03-03 14:30:33 +0000

    Add atcoder/abc107/abc107_a/10513179.cpp

---
+++ atcoder/abc107/abc107_a/10513179.cpp
@@ -0,0 +1,12 @@
+// URL: https://atcoder.jp/contests/abc107/submissions/10513179
+// Date: Tue, 03 Mar 2020 14:30:33 +0000
+// Language: C++14 (GCC 5.4.1)
+// Copyright 2020 yaito3014
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int N, i;
+	cin >> N >> i;
+	cout << (N + 1 - i) << endl;
+}
