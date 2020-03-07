129ccb51023f662979d94b38b35a94e221d035f4
yaito3014 <ykakeyama3014@gmail.com>
2020-03-07 12:02:21 +0000

    Add atcoder/abc158/abc158_a/10589543.cpp

---
+++ atcoder/abc158/abc158_a/10589543.cpp
@@ -0,0 +1,13 @@
+// URL: https://atcoder.jp/contests/abc158/submissions/10589543
+// Date: Sat, 07 Mar 2020 12:02:21 +0000
+// Language: C++14 (GCC 5.4.1)
+// Copyright 2020 yaito3014
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  cin.tie(nullptr); ios::sync_with_stdio(false);
+  string S;
+  cin >> S;
+  cout << (S == "AAA" || S == "BBB" ? "No" : "Yes") << endl;
+}
