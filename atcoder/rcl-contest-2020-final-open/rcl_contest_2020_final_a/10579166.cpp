180beb9649053c308e0c93d01514cf010c8eacc8
yaito3014 <ykakeyama3014@gmail.com>
2020-03-07 06:23:46 +0000

    Add atcoder/rcl-contest-2020-final-open/rcl_contest_2020_final_a/10579166.cpp

---
+++ atcoder/rcl-contest-2020-final-open/rcl_contest_2020_final_a/10579166.cpp
@@ -0,0 +1,61 @@
+// URL: https://atcoder.jp/contests/rcl-contest-2020-final-open/submissions/10579166
+// Date: Sat, 07 Mar 2020 06:23:46 +0000
+// Language: C++14 (GCC 5.4.1)
+// Copyright 2020 yaito3014
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  cin.tie(nullptr); ios::sync_with_stdio(false);
+  int N, M;
+  cin >> N >> M;
+  array<int, 6> dice_count, dice = { { 1, 2, 3, 4, 5, 6 } };
+  dice_count.fill(1);
+  auto modify = [&](int index, int value) {
+    int old = dice[index];
+    --dice_count[dice[index] - 1];
+    ++dice_count[value - 1];
+    dice[index] = value;
+    return old;
+  };
+  vector<int> field(N + 1, 0);
+  field[N] = 5000;
+  int read_count = 0;
+  auto trans = [&](int cur) {
+    pair<int, int> res{ 0,dice[0] };
+    double max_score = 0;
+    for (int i = 0; i < 6; ++i) {
+      for (int j = 1; j <= 6; ++j) {
+        int orig = modify(i, j);
+        double score = 0;
+        for (int k = 0; k < 6; ++k) {
+          int nxt = cur + k + 1;
+          if (nxt > N)nxt = 2 * N - nxt;
+          score += dice_count[k] * field[nxt];
+        }
+        score /= 6;
+        if (score > max_score) {
+          res = make_pair(i, j);
+          max_score = score;
+        }
+        modify(i, orig);
+      }
+    }
+    modify(res.first, res.second);
+  };
+  for (int i = 0; i < M; ++i) {
+    for (int j = 0; j < 6; ++j)
+      cout << dice[j] << " ";
+    cout << endl;
+    //for (int j = 0; j < 6; ++j)
+    //  cerr << dice_count[j] << " ";
+    //cerr << endl;
+    int d, v, x;
+    cin >> d >> v >> x;
+    field[x] = v;
+    if (i < 5)modify(i + 1, 1);
+    else if (read_count < N)++read_count;
+    else trans(x);
+
+  }
+}
