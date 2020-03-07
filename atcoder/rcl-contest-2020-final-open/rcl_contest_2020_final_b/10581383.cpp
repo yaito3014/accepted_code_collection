637e65179020f6adbbe995823ad1fef2a9b8ee81
yaito3014 <ykakeyama3014@gmail.com>
2020-03-07 07:40:21 +0000

    Add atcoder/rcl-contest-2020-final-open/rcl_contest_2020_final_b/10581383.cpp

---
+++ atcoder/rcl-contest-2020-final-open/rcl_contest_2020_final_b/10581383.cpp
@@ -0,0 +1,20 @@
+// URL: https://atcoder.jp/contests/rcl-contest-2020-final-open/submissions/10581383
+// Date: Sat, 07 Mar 2020 07:40:21 +0000
+// Language: C++14 (GCC 5.4.1)
+// Copyright 2020 yaito3014
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  cin.tie(nullptr); ios::sync_with_stdio(false);
+  int N, P, M;
+  cin >> N >> P >> M;
+  vector<string> rows(N), sheets(N);
+  for (auto& row : rows)cin >> row;
+  for (auto& row : sheets)cin >> row;
+  const string s = "UDLR";
+  for (int i = 0; i < M; ++i) {
+    cout << s[i % 4] << '
+';
+  }
+}
