abcbd76972f075fd92f49d0b2b2af137b3392fe7
yaito3014 <ykakeyama3014@gmail.com>
2020-03-08 06:47:01 +0000

    Add atcoder/abc037/abc037_b/10660150.cpp

---
+++ atcoder/abc037/abc037_b/10660150.cpp
@@ -0,0 +1,20 @@
+// URL: https://atcoder.jp/contests/abc037/submissions/10660150
+// Date: Sun, 08 Mar 2020 06:47:01 +0000
+// Language: C++14 (GCC 5.4.1)
+// Copyright 2020 yaito3014
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  cin.tie(nullptr); ios::sync_with_stdio(false);
+  int N, Q;
+  cin >> N >> Q;
+  vector<int> a(N);
+  for (int i = 0; i < Q; ++i) {
+    int L, R, T;
+    cin >> L >> R >> T, --L;
+    for (int j = L; j < R; ++j)a[j] = T;
+  }
+  for (const auto& e : a)cout << e << '
+';
+}
