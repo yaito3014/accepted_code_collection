0b84e586d9f9130be2adda286c0aff42cc4225ce
yaito3014 <ykakeyama3014@gmail.com>
2020-03-11 13:05:29 +0000

    Add atcoder/abs/abc083_b/10759917.rs

---
+++ atcoder/abs/abc083_b/10759917.rs
@@ -0,0 +1,64 @@
+// URL: https://atcoder.jp/contests/abs/submissions/10759917
+// Date: Wed, 11 Mar 2020 13:05:29 +0000
+// Language: Rust (1.15.1)
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let s = {
+            use std::io::Read;
+            let mut s = String::new();
+            std::io::stdin().read_to_string(&mut s).unwrap();
+            s
+        };
+        let mut iter = s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($iter:expr) => {};
+    ($iter:expr, ) => {};
+
+    ($iter:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($iter, $t);
+        input_inner!{$iter $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($iter:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($iter, $t)),* )
+    };
+
+    ($iter:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($iter, $t)).collect::<Vec<_>>()
+    };
+
+    ($iter:expr, chars) => {
+        read_value!($iter, String).chars().collect::<Vec<char>>()
+    };
+
+    ($iter:expr, usize1) => {
+        read_value!($iter, usize) - 1
+    };
+
+    ($iter:expr, $t:ty) => {
+        $iter.next().unwrap().parse::<$t>().expect("Parse error")
+    };
+}
+
+fn main() {
+    input! { n:i32, a: i32, b: i32 }
+    let (n, a, b): (i32, i32, i32) = (n, a, b);
+    let mut ans = 0;
+    for x in 1..n + 1 {
+        let sum = x.to_string().chars().map(|s| s as i32 - '0' as i32).sum::<i32>();
+        if a <= sum && sum <= b {
+            ans += x;
+        }
+    }
+    println!("{}", ans);
+}
