6028c3624410cb95648bb322ab7f857609010244
yaito3014 <ykakeyama3014@gmail.com>
2020-03-20 00:56:39 +0000

    Add atcoder/abc158/abc158_a/11013898.c

---
+++ atcoder/abc158/abc158_a/11013898.c
@@ -0,0 +1,4 @@
+// URL: https://atcoder.jp/contests/abc158/submissions/11013898
+// Date: Fri, 20 Mar 2020 00:56:39 +0000
+// Language: C (GCC 5.4.1)
+main(){char s[4];gets(s);puts(*s-s[1]||s[1]-s[2]?"Yes":"No");}
