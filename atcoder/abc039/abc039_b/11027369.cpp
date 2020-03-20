e79bef6289070b18eca6a6eceb32db9caa9038ad
yaito3014 <ykakeyama3014@gmail.com>
2020-03-20 13:47:59 +0000

    Add atcoder/abc039/abc039_b/11027369.cpp

---
+++ atcoder/abc039/abc039_b/11027369.cpp
@@ -0,0 +1,13 @@
+// URL: https://atcoder.jp/contests/abc039/submissions/11027369
+// Date: Fri, 20 Mar 2020 13:47:59 +0000
+// Language: C++14 (GCC 5.4.1)
+// Copyright 2020 yaito3014
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  cin.tie(nullptr); ios::sync_with_stdio(false);
+  int X;
+  cin >> X;
+  cout << int(sqrt(sqrt(X))) << endl;
+}
