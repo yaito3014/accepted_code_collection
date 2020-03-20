5dae77733bbc6a902d08087f599b8d13ac13e33a
yaito3014 <ykakeyama3014@gmail.com>
2020-03-20 00:45:09 +0000

    Add atcoder/abc158/abc158_a/11013784.c

---
+++ atcoder/abc158/abc158_a/11013784.c
@@ -0,0 +1,4 @@
+// URL: https://atcoder.jp/contests/abc158/submissions/11013784
+// Date: Fri, 20 Mar 2020 00:45:09 +0000
+// Language: C (GCC 5.4.1)
+main(){char s[4];scanf("%s",s);puts(strcmp(s,"AAA")+strcmp(s,"BBB")?"No":"Yes");}
