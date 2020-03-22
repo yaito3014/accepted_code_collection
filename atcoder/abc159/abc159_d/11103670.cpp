9cffec5ccccc6c59fc1c9706349072861f50b76b
yaito3014 <ykakeyama3014@gmail.com>
2020-03-22 12:23:31 +0000

    Add atcoder/abc159/abc159_d/11103670.cpp

---
+++ atcoder/abc159/abc159_d/11103670.cpp
@@ -0,0 +1,23 @@
+// URL: https://atcoder.jp/contests/abc159/submissions/11103670
+// Date: Sun, 22 Mar 2020 12:23:31 +0000
+// Language: C++14 (GCC 5.4.1)
+// Copyright 2020 yaito3014
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  cin.tie(nullptr); ios::sync_with_stdio(false);
+  int N;
+  cin >> N;
+  vector<int> A(N);
+  for (auto& e : A)cin >> e;
+  vector<int64_t> count(N, 0);
+  for (const auto& e : A)++count[e - 1];
+  int64_t ans = 0;
+  for (const auto& e : count)
+    ans += e * (e - 1) / 2;
+  for (const auto& e : A) {
+    cout << (ans - count[e - 1] * (count[e - 1] - 1) / 2 + (count[e - 1] - 1) * (count[e - 1] - 2) / 2) << "
+";
+  }
+}
