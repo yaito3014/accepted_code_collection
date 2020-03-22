f7d61cf7ab0d6b694812efb70004b2b7a8e31cd1
ecto0310 <ecto0310@gmail.com>
2020-03-22 12:09:41 +0000

    Add atcoder/abc159/abc159_c/11092329.cpp

---
+++ atcoder/abc159/abc159_c/11092329.cpp
@@ -0,0 +1,16 @@
+// URL: https://atcoder.jp/contests/abc159/submissions/11092329
+// Date: Sun, 22 Mar 2020 12:09:41 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl "
+"
+
+int main()
+{
+  i64 L;
+  cin >> L;
+  cout << fixed << setprecision(10) << pow((double)L / 3, 3) << endl;
+  return 0;
+}
