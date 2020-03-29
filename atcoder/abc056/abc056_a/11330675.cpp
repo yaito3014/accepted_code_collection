73049d25f59de3b195769ef68a053a95ce5b44e1
yaito3014 <ykakeyama3014@gmail.com>
2020-03-29 02:32:10 +0000

    Add atcoder/abc056/abc056_a/11330675.cpp

---
+++ atcoder/abc056/abc056_a/11330675.cpp
@@ -0,0 +1,13 @@
+// URL: https://atcoder.jp/contests/abc056/submissions/11330675
+// Date: Sun, 29 Mar 2020 02:32:10 +0000
+// Language: C++14 (GCC 5.4.1)
+// Copyright 2020 yaito3014
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  cin.tie(nullptr); ios::sync_with_stdio(false);
+  char a, b;
+  cin >> a >> b;
+  cout << (a == b ? 'H' : 'D') << endl;
+}
