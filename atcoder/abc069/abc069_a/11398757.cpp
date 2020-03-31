e98140b202d575305a2b34d39061873e550e36ea
yaito3014 <ykakeyama3014@gmail.com>
2020-03-31 14:43:57 +0000

    Add atcoder/abc069/abc069_a/11398757.cpp

---
+++ atcoder/abc069/abc069_a/11398757.cpp
@@ -0,0 +1,12 @@
+// URL: https://atcoder.jp/contests/abc069/submissions/11398757
+// Date: Tue, 31 Mar 2020 14:43:57 +0000
+// Language: C++14 (GCC 5.4.1)
+// Copyright 2020 yaito3014
+#include <bits/stdc++.h>
+
+int main() {
+  std::cin.tie(nullptr); std::ios::sync_with_stdio(false);
+  int n, m;
+  std::cin >> n >> m;
+  std::cout << (n - 1) * (m - 1) << std::endl;
+}
