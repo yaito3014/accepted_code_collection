d9f323406945e74831fa4ef3b80195d14882c8a1
yaito3014 <ykakeyama3014@gmail.com>
2020-03-31 15:14:18 +0000

    Add atcoder/abc072/abc072_a/11399440.cpp

---
+++ atcoder/abc072/abc072_a/11399440.cpp
@@ -0,0 +1,12 @@
+// URL: https://atcoder.jp/contests/abc072/submissions/11399440
+// Date: Tue, 31 Mar 2020 15:14:18 +0000
+// Language: C++14 (GCC 5.4.1)
+// Copyright 2020 yaito3014
+#include <bits/stdc++.h>
+
+int main() {
+  std::cin.tie(nullptr); std::ios::sync_with_stdio(false);
+  int X, t;
+  std::cin >> X >> t;
+  std::cout << std::max(0, X - t) << std::endl;
+}
