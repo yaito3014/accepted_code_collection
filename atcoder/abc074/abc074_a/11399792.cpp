d1b41164137198800a9c24042924a2d587c71998
yaito3014 <ykakeyama3014@gmail.com>
2020-03-31 15:31:59 +0000

    Add atcoder/abc074/abc074_a/11399792.cpp

---
+++ atcoder/abc074/abc074_a/11399792.cpp
@@ -0,0 +1,12 @@
+// URL: https://atcoder.jp/contests/abc074/submissions/11399792
+// Date: Tue, 31 Mar 2020 15:31:59 +0000
+// Language: C++14 (GCC 5.4.1)
+// Copyright 2020 yaito3014
+#include <bits/stdc++.h>
+
+int main() {
+  std::cin.tie(nullptr); std::ios::sync_with_stdio(false);
+  int N, A;
+  std::cin >> N >> A;
+  std::cout << (N * N - A) << std::endl;
+}
