9719786f7a26ec6dae2e7980cba2bd60eb247a41
ecto0310 <ecto0310@gmail.com>
2020-03-31 15:15:22 +0000

    Add atcoder/otemae2019/otemae2019_a/11399464.cpp

---
+++ atcoder/otemae2019/otemae2019_a/11399464.cpp
@@ -0,0 +1,19 @@
+// URL: https://atcoder.jp/contests/otemae2019/submissions/11399464
+// Date: Tue, 31 Mar 2020 15:15:22 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl "
+"
+
+int main()
+{
+  i64 A, B;
+  cin >> A >> B;
+  if (B <= A)
+    cout << 1 << endl;
+  else
+    cout << 0 << endl;
+  return 0;
+}
