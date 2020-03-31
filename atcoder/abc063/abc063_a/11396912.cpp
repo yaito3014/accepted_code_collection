91666d6d456d00bf6e64fc6d9e4db85ec1207f9d
yaito3014 <ykakeyama3014@gmail.com>
2020-03-31 13:31:15 +0000

    Add atcoder/abc063/abc063_a/11396912.cpp

---
+++ atcoder/abc063/abc063_a/11396912.cpp
@@ -0,0 +1,14 @@
+// URL: https://atcoder.jp/contests/abc063/submissions/11396912
+// Date: Tue, 31 Mar 2020 13:31:15 +0000
+// Language: C++14 (GCC 5.4.1)
+// Copyright 2020 yaito3014
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  cin.tie(nullptr); ios::sync_with_stdio(false);
+  int A, B;
+  cin >> A >> B;
+  if (A + B >= 10)cout << "error" << endl;
+  else cout << (A + B) << endl;
+}
