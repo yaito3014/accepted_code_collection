2d6288abfa6338431e6145d719fc845291b55f5a
yaito3014 <ykakeyama3014@gmail.com>
2020-04-03 02:08:52 +0000

    Add atcoder/abs/abc088_b/11452289.py

---
+++ atcoder/abs/abc088_b/11452289.py
@@ -0,0 +1,4 @@
+// URL: https://atcoder.jp/contests/abs/submissions/11452289
+// Date: Fri, 03 Apr 2020 02:08:52 +0000
+// Language: Python (3.4.3)
+[[print(sum(a)-2*sum(a[1::2]))for a in[sorted(list(map(int,input().split())),reverse=True)]]for _ in[input()]]
