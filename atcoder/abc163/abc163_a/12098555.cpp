b11e670f1a8c9a7b7a8d2a247a5d15f63d7002f8
yaito3014 <ykakeyama3014@gmail.com>
2020-04-19 12:08:57 +0000

    Add atcoder/abc163/abc163_a/12098555.cpp

---
+++ atcoder/abc163/abc163_a/12098555.cpp
@@ -0,0 +1,13 @@
+// URL: https://atcoder.jp/contests/abc163/submissions/12098555
+// Date: Sun, 19 Apr 2020 12:08:57 +0000
+// Language: C++ (GCC 9.2.1)
+// Copyright 2020 yaito3014
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  cin.tie(nullptr); ios::sync_with_stdio(false);
+  int R;
+  cin >> R;
+  cout << fixed << setprecision(3) << 2 * R * 3.14159265 << endl;
+}
