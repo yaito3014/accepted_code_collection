e9fed71639af87945268b837fc562cc30054eae8
yaito3014 <ykakeyama3014@gmail.com>
2020-04-19 12:12:33 +0000

    Add atcoder/abc163/abc163_c/12107520.cpp

---
+++ atcoder/abc163/abc163_c/12107520.cpp
@@ -0,0 +1,20 @@
+// URL: https://atcoder.jp/contests/abc163/submissions/12107520
+// Date: Sun, 19 Apr 2020 12:12:33 +0000
+// Language: C++ (GCC 9.2.1)
+// Copyright 2020 yaito3014
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  cin.tie(nullptr); ios::sync_with_stdio(false);
+  int N;
+  cin >> N;
+  vector<int> ans(N);
+  for (int i = 1; i < N; ++i) {
+    int A;
+    cin >> A, --A;
+    ++ans[A];
+  }
+  for (auto& e : ans)cout << e << '
+';
+}
