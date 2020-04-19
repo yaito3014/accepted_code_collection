0ae8dcd001815336a189ea9f15e9c4e5dfb122d1
ecto0310 <ecto0310@gmail.com>
2020-04-19 12:03:34 +0000

    Add atcoder/abc163/abc163_a/12082035.cpp

---
+++ atcoder/abc163/abc163_a/12082035.cpp
@@ -0,0 +1,18 @@
+// URL: https://atcoder.jp/contests/abc163/submissions/12082035
+// Date: Sun, 19 Apr 2020 12:03:34 +0000
+// Language: C++ (GCC 9.2.1)
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl "
+"
+
+const double PI = 3.1415926535;
+
+int main()
+{
+  i64 R;
+  cin >> R;
+  cout << fixed << setprecision(3) << (double)R * 2.0 * PI << endl;
+  return 0;
+}
