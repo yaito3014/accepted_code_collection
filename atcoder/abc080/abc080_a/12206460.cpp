ae16ab5bcb5f3a2863692884034b24cf49fa6274
yaito3014 <ykakeyama3014@gmail.com>
2020-04-21 18:07:52 +0000

    Add atcoder/abc080/abc080_a/12206460.cpp

---
+++ atcoder/abc080/abc080_a/12206460.cpp
@@ -0,0 +1,13 @@
+// URL: https://atcoder.jp/contests/abc080/submissions/12206460
+// Date: Tue, 21 Apr 2020 18:07:52 +0000
+// Language: C++14 (GCC 5.4.1)
+// Copyright 2020 yaito3014
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	cin.tie(nullptr); ios::sync_with_stdio(false);
+	int N, A, B;
+	cin >> N >> A >> B;
+	cout << min(N * A, B) << endl;
+}
