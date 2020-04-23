d8b8fa855b19c2adab591f35774d9cae4fe7e1eb
yaito3014 <ykakeyama3014@gmail.com>
2020-04-23 17:11:10 +0000

    Add atcoder/abc082/abc082_a/12255656.cpp

---
+++ atcoder/abc082/abc082_a/12255656.cpp
@@ -0,0 +1,13 @@
+// URL: https://atcoder.jp/contests/abc082/submissions/12255656
+// Date: Thu, 23 Apr 2020 17:11:10 +0000
+// Language: C++14 (GCC 5.4.1)
+// Copyright 2020 yaito3014
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	cin.tie(nullptr); ios::sync_with_stdio(false);
+	int a, b;
+	cin >> a >> b;
+	cout << ((a+b)/2+(a+b)%2) << endl;
+}
