12a6e55f7f7abf0f921474a2e40e030624d2e03f
yaito3014 <ykakeyama3014@gmail.com>
2020-04-26 12:01:28 +0000

    Add atcoder/abc164/abc164_a/12334953.cpp

---
+++ atcoder/abc164/abc164_a/12334953.cpp
@@ -0,0 +1,13 @@
+// URL: https://atcoder.jp/contests/abc164/submissions/12334953
+// Date: Sun, 26 Apr 2020 12:01:28 +0000
+// Language: C++ (GCC 9.2.1)
+// Copyright 2020 yaito3014
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  cin.tie(nullptr); ios::sync_with_stdio(false);
+  int S, W;
+  cin >> S >> W;
+  cout << (W >= S ? "unsafe" : "safe") << endl;
+}
