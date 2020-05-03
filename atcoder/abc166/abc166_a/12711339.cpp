2e099aa6d8a3d7121c8c99e1c2617b711ad24859
yaito3014 <ykakeyama3014@gmail.com>
2020-05-03 12:01:44 +0000

    Add atcoder/abc166/abc166_a/12711339.cpp

---
+++ atcoder/abc166/abc166_a/12711339.cpp
@@ -0,0 +1,13 @@
+// URL: https://atcoder.jp/contests/abc166/submissions/12711339
+// Date: Sun, 03 May 2020 12:01:44 +0000
+// Language: C++ (GCC 9.2.1)
+// Copyright 2020 yaito3014
+#include <bits/stdc++.h>
+
+int main() {
+  using namespace std;
+  cin.tie(nullptr); ios::sync_with_stdio(false);
+  string S;
+  cin >> S;
+  cout << (S == "ABC" ? "ARC" : "ABC") << endl;
+}
