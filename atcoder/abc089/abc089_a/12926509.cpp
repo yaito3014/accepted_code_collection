e3c758e175ec78ff401b19975cb2be4a2d255eae
yaito3014 <ykakeyama3014@gmail.com>
2020-05-07 12:00:52 +0000

    Add atcoder/abc089/abc089_a/12926509.cpp

---
+++ atcoder/abc089/abc089_a/12926509.cpp
@@ -0,0 +1,10 @@
+// URL: https://atcoder.jp/contests/abc089/submissions/12926509
+// Date: Thu, 07 May 2020 12:00:52 +0000
+// Language: C++14 (GCC 5.4.1)
+#include <iostream>
+
+int main() {
+  int N;
+  std::cin >> N;
+  std::cout << (N / 3) << std::endl;
+}
