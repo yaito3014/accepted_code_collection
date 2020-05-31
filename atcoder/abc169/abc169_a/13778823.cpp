562bdc569d1c14ccc18d5f334f814fb80f0cf36b
yaito3014 <ykakeyama3014@gmail.com>
2020-05-31 12:00:57 +0000

    Add atcoder/abc169/abc169_a/13778823.cpp

---
+++ atcoder/abc169/abc169_a/13778823.cpp
@@ -0,0 +1,14 @@
+// URL: https://atcoder.jp/contests/abc169/submissions/13778823
+// Date: Sun, 31 May 2020 12:00:57 +0000
+// Language: C++ (GCC 9.2.1)
+// Copyright 2020 yaito3014
+#include <bits/stdc++.h>
+
+int main() {
+  using namespace std;
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+  int A, B;
+  cin >> A >> B;
+  cout << (A * B) << endl;
+}
