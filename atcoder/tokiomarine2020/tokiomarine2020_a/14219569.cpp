133545d5f9f5e192320770f474460776e64fceae
yaito3014 <ykakeyama3014@gmail.com>
2020-06-13 12:01:04 +0000

    Add atcoder/tokiomarine2020/tokiomarine2020_a/14219569.cpp

---
+++ atcoder/tokiomarine2020/tokiomarine2020_a/14219569.cpp
@@ -0,0 +1,14 @@
+// URL: https://atcoder.jp/contests/tokiomarine2020/submissions/14219569
+// Date: Sat, 13 Jun 2020 12:01:04 +0000
+// Language: C++ (GCC 9.2.1)
+// Copyright 2020 yaito3014
+#include <bits/stdc++.h>
+
+int main() {
+  using namespace std;
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+  string S;
+  cin >> S;
+  cout << S.substr(0, 3) << endl;
+}
