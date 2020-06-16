59a58f0db2176186cfa20eed270387283a04d767
yaito3014 <ykakeyama3014@gmail.com>
2020-06-16 14:53:11 +0000

    Add atcoder/abc099/abc099_a/14417037.cpp

---
+++ atcoder/abc099/abc099_a/14417037.cpp
@@ -0,0 +1,14 @@
+// URL: https://atcoder.jp/contests/abc099/submissions/14417037
+// Date: Tue, 16 Jun 2020 14:53:11 +0000
+// Language: C++14 (GCC 5.4.1)
+// Copyright 2020 yaito3014
+#include <bits/stdc++.h>
+
+int main() {
+  using namespace std;
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+  int N;
+  cin >> N;
+  cout << (N < 1000 ? "ABC" : "ABD") << '\n';
+}
