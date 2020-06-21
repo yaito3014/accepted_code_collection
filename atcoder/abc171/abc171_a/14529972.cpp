1f623afd7fdce54c6add838d0801b023ad9b2257
yaito3014 <ykakeyama3014@gmail.com>
2020-06-21 12:01:57 +0000

    Add atcoder/abc171/abc171_a/14529972.cpp

---
+++ atcoder/abc171/abc171_a/14529972.cpp
@@ -0,0 +1,14 @@
+// URL: https://atcoder.jp/contests/abc171/submissions/14529972
+// Date: Sun, 21 Jun 2020 12:01:57 +0000
+// Language: C++ (GCC 9.2.1)
+// Copyright 2020 yaito3014
+#include <bits/stdc++.h>
+
+int main() {
+  using namespace std;
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+  char c;
+  cin >> c;
+  cout << (c >= 'a' ? 'a' : 'A') << '\n';
+}
