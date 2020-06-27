b0bc1a785411e6a9b1019b9b440e6d2791fb71a8
yaito3014 <ykakeyama3014@gmail.com>
2020-06-27 13:36:44 +0000

    Add atcoder/abc172/abc172_a/14774832.cpp

---
+++ atcoder/abc172/abc172_a/14774832.cpp
@@ -0,0 +1,14 @@
+// URL: https://atcoder.jp/contests/abc172/submissions/14774832
+// Date: Sat, 27 Jun 2020 13:36:44 +0000
+// Language: C++ (GCC 9.2.1)
+// Copyright 2020 yaito3014
+#include <bits/stdc++.h>
+
+int main() {
+  using namespace std;
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+  int a;
+  cin >> a;
+  cout << (a + a * a + a * a * a) << '\n';
+}
