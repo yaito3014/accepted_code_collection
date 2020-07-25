2e522fd5e7a5efab53aa6712d7d6211bb4f9c02d
yaito3014 <ykakeyama3014@gmail.com>
2020-07-25 12:32:11 +0000

    Add atcoder/m-solutions2020/m_solutions2020_a/15433709.cpp

---
+++ atcoder/m-solutions2020/m_solutions2020_a/15433709.cpp
@@ -0,0 +1,11 @@
+// URL: https://atcoder.jp/contests/m-solutions2020/submissions/15433709
+// Date: Sat, 25 Jul 2020 12:32:11 +0000
+// Language: C++ (GCC 9.2.1)
+// Copyright 2020 yaito3014
+#include <iostream>
+
+int main() {
+  int X;
+  std::cin >> X;
+  std::cout << (10 - X / 200) << '\n';
+}
