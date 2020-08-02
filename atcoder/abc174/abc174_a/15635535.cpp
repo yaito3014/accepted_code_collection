e7f8a1d60bc795b5801a2932b324b9fd62c3c080
yaito3014 <ykakeyama3014@gmail.com>
2020-08-02 13:27:00 +0000

    Add atcoder/abc174/abc174_a/15635535.cpp

---
+++ atcoder/abc174/abc174_a/15635535.cpp
@@ -0,0 +1,11 @@
+// URL: https://atcoder.jp/contests/abc174/submissions/15635535
+// Date: Sun, 02 Aug 2020 13:27:00 +0000
+// Language: C++ (GCC 9.2.1)
+// Copyright 2020 yaito3014
+#include <iostream>
+
+int main() {
+  int X;
+  std::cin >> X;
+  std::cout << (X >= 30 ? "Yes" : "No") << '\n';
+}
