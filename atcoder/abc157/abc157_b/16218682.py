db29394ef6a0d9ee2353e74d8a668b603a2bbfde
yaito3014 <ykakeyama3014@gmail.com>
2020-08-25 06:46:45 +0000

    Add atcoder/abc157/abc157_b/16218682.py

---
+++ atcoder/abc157/abc157_b/16218682.py
@@ -0,0 +1,16 @@
+// URL: https://atcoder.jp/contests/abc157/submissions/16218682
+// Date: Tue, 25 Aug 2020 06:46:45 +0000
+// Language: Python (3.8.2)
+A = [list(map(int, input().split())) for _ in range(3)]
+N = int(input())
+b = [int(input()) for _ in range(N)]
+
+f=0
+for v in b:
+	for i in range(9):
+		if A[i // 3][i % 3] == v:
+			f|=1<<i
+ans = 0
+for v in [7,56,73,84,146,273,292,448]:
+  ans |= f&v==v;
+print('Yes' if ans else 'No')
