e8e8b05461697b60e9fbd4e7428d583bf71419a8
yaito3014 <ykakeyama3014@gmail.com>
2020-10-03 12:01:20 +0000

    Add atcoder/arc104/arc104_a/17156385.cpp

---
+++ atcoder/arc104/arc104_a/17156385.cpp
@@ -0,0 +1,11 @@
+// URL: https://atcoder.jp/contests/arc104/submissions/17156385
+// Date: Sat, 03 Oct 2020 12:01:20 +0000
+// Language: C++ (GCC 9.2.1)
+// Copyright 2020 yaito3014
+#include <iostream>
+
+int main() {
+  int A, B;
+  std::cin >> A >> B;
+  std::cout << (A + B) / 2 << ' ' << (A - B) / 2 << '\n';
+}
