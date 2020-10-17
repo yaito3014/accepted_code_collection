030fdf8bb2caa2aac19a5bd07ff8e6c494e73864
yaito3014 <ykakeyama3014@gmail.com>
2020-10-17 11:01:34 +0000

    Add atcoder/abc180/abc180_a/17439348.cpp

---
+++ atcoder/abc180/abc180_a/17439348.cpp
@@ -0,0 +1,10 @@
+// URL: https://atcoder.jp/contests/abc180/submissions/17439348
+// Date: Sat, 17 Oct 2020 11:01:34 +0000
+// Language: C++ (GCC 9.2.1)
+#include <iostream>
+
+int main() {
+  int N, A, B;
+  std::cin >> N >> A >> B;
+  std::cout << (N - A + B) << '\n';
+}
