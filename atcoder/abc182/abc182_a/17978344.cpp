b42e38d50bada927b68520212427319471ce7128
yaito3014 <ykakeyama3014@gmail.com>
2020-11-08 12:49:54 +0000

    Add atcoder/abc182/abc182_a/17978344.cpp

---
+++ atcoder/abc182/abc182_a/17978344.cpp
@@ -0,0 +1,11 @@
+// URL: https://atcoder.jp/contests/abc182/submissions/17978344
+// Date: Sun, 08 Nov 2020 12:49:54 +0000
+// Language: C++ (GCC 9.2.1)
+#include <iostream>
+#include <string>
+
+int main() {
+  int A, B;
+  std::cin >> A >> B;
+  std::cout << (2 * A + 100 - B) << '\n';
+}
