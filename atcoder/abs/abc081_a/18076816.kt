1721564bbbe7cb4bf16469e242322e17be8bbc35
yaito3014 <ykakeyama3014@gmail.com>
2020-11-13 09:21:46 +0000

    Add atcoder/abs/abc081_a/18076816.kt

---
+++ atcoder/abs/abc081_a/18076816.kt
@@ -0,0 +1,4 @@
+// URL: https://atcoder.jp/contests/abs/submissions/18076816
+// Date: Fri, 13 Nov 2020 09:21:46 +0000
+// Language: Kotlin (1.3.71)
+fun main() = println(readLine()!!.count { it == '1' })
