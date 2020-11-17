be812761f568c6b0dbcb6e1d0cc9db01d865e6c6
yaito3014 <ykakeyama3014@gmail.com>
2020-11-17 22:52:15 +0000

    Add atcoder/abc183/abc183_a/18196930.cpp

---
+++ atcoder/abc183/abc183_a/18196930.cpp
@@ -0,0 +1,11 @@
+// URL: https://atcoder.jp/contests/abc183/submissions/18196930
+// Date: Tue, 17 Nov 2020 22:52:15 +0000
+// Language: C++ (GCC 9.2.1)
+#include <iostream>
+#include <numeric>
+
+int main() {
+  int x;
+  std::cin >> x;
+  std::cout << std::max(0, x) << '\n';
+}
