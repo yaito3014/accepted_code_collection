79c86435ccdf9186e3444b1f7018f2fe3ef413e2
yaito3014 <ykakeyama3014@gmail.com>
2020-11-22 12:01:03 +0000

    Add atcoder/abc184/abc184_a/18305046.cpp

---
+++ atcoder/abc184/abc184_a/18305046.cpp
@@ -0,0 +1,10 @@
+// URL: https://atcoder.jp/contests/abc184/submissions/18305046
+// Date: Sun, 22 Nov 2020 12:01:03 +0000
+// Language: C++ (GCC 9.2.1)
+#include <iostream>
+
+int main() {
+  int a, b, c, d;
+  std::cin >> a >> b >> c >> d;
+  std::cout << (a * d - b * c) << '\n';
+}
