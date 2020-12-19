90fc628a735ba0cd86d7fcba0c7778cf7bce298c
yaito3014 <ykakeyama3014@gmail.com>
2020-12-19 12:32:38 +0000

    Add atcoder/abc186/abc186_a/18878961.cpp

---
+++ atcoder/abc186/abc186_a/18878961.cpp
@@ -0,0 +1,12 @@
+// URL: https://atcoder.jp/contests/abc186/submissions/18878961
+// Date: Sat, 19 Dec 2020 12:32:38 +0000
+// Language: C++ (GCC 9.2.1)
+#include <cstdint>
+#include <iostream>
+
+int main() {
+  using u32 = uint_fast32_t;
+  u32 n, w;
+  std::cin >> n >> w;
+  std::cout << (n / w) << '\n';
+}
