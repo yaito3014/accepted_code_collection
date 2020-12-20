ec8849105a27e0a93671bc44cd53bb4697179ece
yaito3014 <ykakeyama3014@gmail.com>
2020-12-20 06:04:30 +0000

    Add atcoder/abc084/abc084_a/18904337.cpp

---
+++ atcoder/abc084/abc084_a/18904337.cpp
@@ -0,0 +1,25 @@
+// URL: https://atcoder.jp/contests/abc084/submissions/18904337
+// Date: Sun, 20 Dec 2020 06:04:30 +0000
+// Language: C++ (GCC 9.2.1)
+#include <algorithm>
+#include <cmath>
+#include <cstdint>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <limits>
+#include <map>
+#include <numeric>
+#include <set>
+#include <string>
+#include <vector>
+
+int main() {
+  using i32 = int32_t;
+  using u32 = uint32_t;
+  using i64 = int64_t;
+  using u64 = uint64_t;
+  u32 m;
+  std::cin >> m;
+  std::cout << (48 - m) << '\n';
+}
