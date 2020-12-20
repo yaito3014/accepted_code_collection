dabee43040873de3686e2a94d2663cd906e3747b
yaito3014 <ykakeyama3014@gmail.com>
2020-12-20 05:27:37 +0000

    Add atcoder/abc169/abc169_a/18903718.cpp

---
+++ atcoder/abc169/abc169_a/18903718.cpp
@@ -0,0 +1,25 @@
+// URL: https://atcoder.jp/contests/abc169/submissions/18903718
+// Date: Sun, 20 Dec 2020 05:27:37 +0000
+// Language: C++ (GCC 9.2.1)
+#include <algorithm>
+#include <cmath>
+#include <cstdint>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <limits>
+#include <map>
+#include <numeric>
+#include <set>
+#include <string>
+#include <vector>
+
+int main() {
+  using i32 = int32_t;
+  using u32 = uint32_t;
+  using i64 = int64_t;
+  using u64 = uint64_t;
+  u32 a, b;
+  std::cin >> a >> b;
+  std::cout << (a * b) << '\n';
+}
