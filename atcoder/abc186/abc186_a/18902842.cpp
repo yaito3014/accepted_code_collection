79aa721f57652a2bfc6e29e8fc44cde7cea21404
yaito3014 <ykakeyama3014@gmail.com>
2020-12-20 04:29:57 +0000

    Add atcoder/abc186/abc186_a/18902842.cpp

---
+++ atcoder/abc186/abc186_a/18902842.cpp
@@ -0,0 +1,22 @@
+// URL: https://atcoder.jp/contests/abc186/submissions/18902842
+// Date: Sun, 20 Dec 2020 04:29:57 +0000
+// Language: C++ (GCC 9.2.1)
+#include <algorithm>
+#include <cmath>
+#include <cstdint>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <limits>
+#include <numeric>
+#include <vector>
+
+int main() {
+  using i32 = int32_t;
+  using u32 = uint32_t;
+  using i64 = int64_t;
+  using u64 = uint64_t;
+  u32 n, w;
+  std::cin >> n >> w;
+  std::cout << (n / w) << '\n';
+}
