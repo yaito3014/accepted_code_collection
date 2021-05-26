262f1cc1d927c9a5e24dc78405db6d86b3d86f8b
yaito3014 <ykakeyama3014@gmail.com>
2021-05-26 17:55:14 +0000

    Add atcoder/abc121/abc121_c/22937638.cpp

---
+++ atcoder/abc121/abc121_c/22937638.cpp
@@ -0,0 +1,30 @@
+// URL: https://atcoder.jp/contests/abc121/submissions/22937638
+// Date: Wed, 26 May 2021 17:55:14 +0000
+// Language: C++ (GCC 9.2.1)
+#include <algorithm>
+#include <cstdint>
+#include <iostream>
+#include <iterator>
+#include <utility>
+#include <vector>
+
+int main() {
+  using u32 = uint_fast32_t;
+  using u64 = uint_fast64_t;
+
+  u32 n, m;
+  std::cin >> n >> m;
+
+  std::vector<std::pair<u64, u32>> shops(n);
+  for (auto& [a, b] : shops) std::cin >> a >> b;
+
+  std::sort(std::begin(shops), std::end(shops));
+
+  u64 ans = 0;
+  for (const auto& [a, b] : shops) {
+    ans += a * std::min(m, b);
+    if (m <= b) break;
+    m -= b;
+  }
+  std::cout << ans << std::endl;
+}
