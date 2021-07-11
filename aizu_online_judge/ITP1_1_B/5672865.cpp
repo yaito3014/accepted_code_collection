87bccd743ca3c8d08793fc038e3631c6478cc277
yaito3014 <ykakeyama3014@gmail.com>
2021-07-11 07:03:05 +0000

    Add aizu_online_judge/ITP1_1_B/5672865.cpp

---
+++ aizu_online_judge/ITP1_1_B/5672865.cpp
@@ -0,0 +1,10 @@
+// URL: http://judge.u-aizu.ac.jp/onlinejudge/review.jsp?rid=5672865
+// Date: Sun, 11 Jul 2021 07:03:05 +0000
+// Language: C++17
+#include <iostream>
+
+int main() {
+  int x;
+  std::cin >> x;
+  std::cout << (x * x * x) << '\n';
+}
